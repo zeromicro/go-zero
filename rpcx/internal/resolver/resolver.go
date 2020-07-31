a1c9db0f801230cad863d472b481e58ee02fd8a9
kevin <wanjunfeng@gmail.com>
2020-07-31 17:33:08 +0800

    add resolver

--- rpcx/internal/resolver/resolver.go
+++ rpcx/internal/resolver/resolver.go
@@ -1,13 +1,53 @@
 package resolver
 
-import "google.golang.org/grpc/resolver"
+import (
+	"zero/core/discov"
+
+	"google.golang.org/grpc/resolver"
+)
 
 type discovResolver struct {
-	cc resolver.ClientConn
+	scheme string
+	etcd   discov.EtcdConf
+	cc     resolver.ClientConn
 }
 
-func (r discovResolver) ResolveNow(options resolver.ResolveNowOptions) {
+func (r *discovResolver) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (
+	resolver.Resolver, error) {
+	sub, err := discov.NewSubscriber(r.etcd.Hosts, r.etcd.Key)
+	if err != nil {
+		return nil, err
+	}
+
+	sub.AddListener(func() {
+		vals := sub.Values()
+		var addrs []resolver.Address
+		for _, val := range vals {
+			addrs = append(addrs, resolver.Address{
+				Addr: val,
+			})
+		}
+		r.cc.UpdateState(resolver.State{
+			Addresses: addrs,
+		})
+	})
+
+	return r, nil
 }
 
-func (r discovResolver) Close() {
+func (r *discovResolver) Close() {
+}
+
+func (r *discovResolver) ResolveNow(options resolver.ResolveNowOptions) {
+}
+
+func (r *discovResolver) Scheme() string {
+	return r.scheme
+}
+
+func RegisterResolver(scheme string, etcd discov.EtcdConf) {
+	resolver.Register(&discovResolver{
+		scheme: scheme,
+		etcd:   etcd,
+	})
 }
