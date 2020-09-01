5c0c3ea467395e01246c4621a4921a1fc697d194
kingxt <kingxt4job@gmail.com>
2020-09-01 11:46:10 +0800

    trim space  (#31)
    * rebase upstream
    
    * rebase
    
    * trim no need line
    
    * trim no need line
    
    * trim no need line
    
    * trim space
    
    Co-authored-by: kingxt <dream4kingxt@163.com>

--- tools/goctl/api/util/annotation.go
+++ tools/goctl/api/util/annotation.go
@@ -1,12 +1,16 @@
 package util
 
-import "github.com/tal-tech/go-zero/tools/goctl/api/spec"
+import (
+	"strings"
+
+	"github.com/tal-tech/go-zero/tools/goctl/api/spec"
+)
 
 func GetAnnotationValue(annos []spec.Annotation, key, field string) (string, bool) {
 	for _, anno := range annos {
 		if anno.Name == key {
 			value, ok := anno.Properties[field]
-			return value, ok
+			return strings.TrimSpace(value), ok
 		}
 	}
 	return "", false
