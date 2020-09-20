dc17855367eb0ce1ca1fed04538c8970b1c7ec4e
kevin <wanjunfeng@gmail.com>
2020-09-20 12:08:30 +0800

    printing context key friendly

--- core/trace/tracespec/vars.go
+++ core/trace/tracespec/vars.go
@@ -1,7 +1,12 @@
 package tracespec
 
+// TracingKey is tracing key for context
+var TracingKey = contextKey("X-Trace")
+
 // contextKey a type for context key
 type contextKey string
 
-// TracingKey is tracing key for context
-var TracingKey = contextKey("X-Trace")
+// Printing a context will reveal a fair amount of information about it.
+func (c contextKey) String() string {
+	return "trace/tracespec context key " + string(c)
+}
