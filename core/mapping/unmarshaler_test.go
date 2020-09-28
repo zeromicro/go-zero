e5d7c3ab0444393ace95435a6d7eb02a7030093b
kevin <wanjunfeng@gmail.com>
2020-09-28 15:19:30 +0800

    unmarshal should be struct

--- core/mapping/unmarshaler_test.go
+++ core/mapping/unmarshaler_test.go
@@ -14,6 +14,13 @@ import (
 // so we only can test to 62 bits.
 const maxUintBitsToTest = 62
 
+func TestUnmarshalWithFullNameNotStruct(t *testing.T) {
+	var s map[string]interface{}
+	content := []byte(`{"name":"xiaoming"}`)
+	err := UnmarshalJsonBytes(content, &s)
+	assert.Equal(t, errValueNotStruct, err)
+}
+
 func TestUnmarshalWithoutTagName(t *testing.T) {
 	type inner struct {
 		Optional bool `key:",optional"`
