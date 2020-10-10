4691e2b9ed1f3a4220f67497af67814e0e098c14
anqiansong <anqiansong@xiaoheiban.cn>
2020-10-10 16:05:19 +0800

    add test

--- tools/goctl/rpc/gen_test.go
+++ tools/goctl/rpc/gen_test.go
@@ -5,9 +5,8 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/tal-tech/go-zero/tools/goctl/rpc/ctx"
-	"github.com/tal-tech/go-zero/tools/goctl/rpc/gen"
 	"github.com/tal-tech/go-zero/tools/goctl/rpc/parser"
+	"github.com/tal-tech/go-zero/tools/goctl/util/console"
 )
 
 func TestParseImport(t *testing.T) {
@@ -15,18 +14,7 @@ func TestParseImport(t *testing.T) {
 	base, _ := filepath.Abs("./base.proto")
 	imports, containsAny, err := parser.ParseImport(src)
 	assert.Nil(t, err)
-	assert.Equal(t, false, containsAny)
-	assert.Equal(t, 1, len(imports))
-	assert.Equal(t, "github.com/tal-tech/go-zero/tools/goctl/rpc", imports[0].PbImportName)
-	assert.Equal(t, base, imports[0].OriginalProtoPath)
-}
-
-func TestParseImport2(t *testing.T) {
-	src, _ := filepath.Abs("./test.proto")
-	base, _ := filepath.Abs("./base.proto")
-	imports, containsAny, err := parser.ParseImport(src)
-	assert.Nil(t, err)
-	assert.Equal(t, false, containsAny)
+	assert.Equal(t, true, containsAny)
 	assert.Equal(t, 1, len(imports))
 	assert.Equal(t, "github.com/tal-tech/go-zero/tools/goctl/rpc", imports[0].PbImportName)
 	assert.Equal(t, base, imports[0].OriginalProtoPath)
@@ -35,8 +23,13 @@ func TestParseImport2(t *testing.T) {
 func TestTransfer(t *testing.T) {
 	src, _ := filepath.Abs("./test.proto")
 	abs, _ := filepath.Abs("./test")
-	rpcCtx := ctx.MustCreateRpcContext(src, abs, "", false)
-	g := gen.NewDefaultRpcGenerator(rpcCtx)
-	err := g.Generate()
+	imports, _, _ := parser.ParseImport(src)
+	proto, err := parser.Transfer(src, abs, imports, console.NewConsole(false))
 	assert.Nil(t, err)
+	assert.Equal(t, 1, len(proto.Service))
+	assert.Equal(t, "Greeter", proto.Service[0].Name.Source())
+	assert.Equal(t, 5, len(proto.Structure))
+	data, ok := proto.Structure["map"]
+	assert.Equal(t, true, ok)
+	assert.Equal(t, "M", data.Field[0].Name.Source())
 }
