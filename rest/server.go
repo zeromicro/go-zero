a47bd60a9050ff43062c018695ef995666f6c1f9
kingxt <kingxt4job@gmail.com>
2020-10-19 17:56:21 +0800

    should reverse middlewares

--- rest/server.go
+++ rest/server.go
@@ -104,6 +104,11 @@ func WithJwtTransition(secret, prevSecret string) RouteOption {
 }
 
 func WithMiddlewares(ms []Middleware, rs ...Route) []Route {
+	for i := len(ms)/2 - 1; i >= 0; i-- {
+		opp := len(ms) - 1 - i
+		ms[i], ms[opp] = ms[opp], ms[i]
+	}
+
 	for _, middleware := range ms {
 		rs = WithMiddleware(middleware, rs...)
 	}
