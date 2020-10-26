b6c649fa157144c1a39be0e30bae7d4bf76d9625
anqiansong <anqiansong@xiaoheiban.cn>
2020-10-26 17:59:55 +0800

    replace quote

--- tools/goctl/model/sql/template/delete.go
+++ tools/goctl/model/sql/template/delete.go
@@ -9,9 +9,9 @@ func (m *{{.upperStartCamelObject}}Model) Delete({{.lowerStartCamelPrimaryKey}} 
 
 	{{.keys}}
     _, err {{if .containsIndexCache}}={{else}}:={{end}} m.Exec(func(conn sqlx.SqlConn) (result sql.Result, err error) {
-		query := ` + "`" + `delete from ` + "` +" + ` m.table + ` + " `" + ` where {{.originalPrimaryKey}} = ?` + "`" + `
+		query := "delete from " + m.table +  " where {{.originalPrimaryKey}} = ?"
 		return conn.Exec(query, {{.lowerStartCamelPrimaryKey}})
-	}, {{.keyValues}}){{else}}query := ` + "`" + `delete from ` + "` +" + ` m.table + ` + " `" + ` where {{.originalPrimaryKey}} = ?` + "`" + `
+	}, {{.keyValues}}){{else}}query := "delete from " + m.table + " where {{.originalPrimaryKey}} = ?"
 		_,err:=m.conn.Exec(query, {{.lowerStartCamelPrimaryKey}}){{end}}
 	return err
 }
