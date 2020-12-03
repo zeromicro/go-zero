b403fcb9319d36afefd156e80b9bd99152f9af25
kim <kingxt4job@gmail.com>
2020-12-03 12:47:20 +0800

    add plugin support

--- tools/goctl/plugin/plugin.go
+++ tools/goctl/plugin/plugin.go
@@ -117,21 +117,18 @@ func getCommand(arg string) (string, bool, error) {
 }
 
 func downloadFile(filepath string, url string) error {
-	// Get the data
 	resp, err := http.Get(url)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
 
-	// Create the file
 	out, err := os.Create(filepath)
 	if err != nil {
 		return err
 	}
 	defer out.Close()
 
-	// Write the body to file
 	_, err = io.Copy(out, resp.Body)
 	return err
 }
