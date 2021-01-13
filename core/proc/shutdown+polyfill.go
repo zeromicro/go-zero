37c3b9f5c1d60fd869a74cecd6f8e3ad664c9d9a
Kevin Wan <wanjunfeng@gmail.com>
2021-01-13 18:43:42 +0800

    make sure unlock safe even if listeners panic (#383)
    * make sure unlock safe even if listeners panic
    
    * fix #378
    
    * fix #378

--- core/proc/shutdown+polyfill.go
+++ core/proc/shutdown+polyfill.go
@@ -4,10 +4,12 @@ package proc
 
 import "time"
 
+// AddShutdownListener returns fn itself on windows, lets callers call fn on their own.
 func AddShutdownListener(fn func()) func() {
 	return fn
 }
 
+// AddWrapUpListener returns fn itself on windows, lets callers call fn on their own.
 func AddWrapUpListener(fn func()) func() {
 	return fn
 }
