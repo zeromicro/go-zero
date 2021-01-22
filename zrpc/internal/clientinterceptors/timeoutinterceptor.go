a56994ec321be062b4920cddbfb6d5e7cfe388d6
HarryWang29 <wrz890829@gmail.com>
2021-01-22 18:06:19 +0800

    zrpc client support block

--- zrpc/internal/clientinterceptors/timeoutinterceptor.go
+++ zrpc/internal/clientinterceptors/timeoutinterceptor.go
@@ -8,15 +8,12 @@ import (
 	"google.golang.org/grpc"
 )
 
-const defaultTimeout = time.Second * 2
-
 func TimeoutInterceptor(timeout time.Duration) grpc.UnaryClientInterceptor {
-	if timeout <= 0 {
-		timeout = defaultTimeout
-	}
-
 	return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn,
 		invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+		if timeout == 0 {
+			return invoker(ctx, method, req, reply, cc, opts...)
+		}
 		ctx, cancel := contextx.ShrinkDeadline(ctx, timeout)
 		defer cancel()
 		return invoker(ctx, method, req, reply, cc, opts...)
