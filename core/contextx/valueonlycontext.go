f6894448bdd0da08624489a4f12b7b6850b9e861
Kevin Wan <wanjunfeng@gmail.com>
2021-02-18 18:00:20 +0800

    fix golint issues in core/contextx (#477)

--- core/contextx/valueonlycontext.go
+++ core/contextx/valueonlycontext.go
@@ -21,6 +21,7 @@ func (valueOnlyContext) Err() error {
 	return nil
 }
 
+// ValueOnlyFrom takes all values from the given ctx, without deadline and error control.
 func ValueOnlyFrom(ctx context.Context) context.Context {
 	return valueOnlyContext{
 		Context: ctx,
