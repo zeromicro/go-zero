428498b5a83050544edb58e3359cd49ba4f970a1
kevin <wanjunfeng@gmail.com>
2021-02-18 22:09:42 +0800

    fix golint issues in core/discov

--- core/discov/config.go
+++ core/discov/config.go
@@ -2,11 +2,13 @@ package discov
 
 import "errors"
 
+// EtcdConf is the config item with the given key on etcd.
 type EtcdConf struct {
 	Hosts []string
 	Key   string
 }
 
+// Validate validates c.
 func (c EtcdConf) Validate() error {
 	if len(c.Hosts) == 0 {
 		return errors.New("empty etcd hosts")
