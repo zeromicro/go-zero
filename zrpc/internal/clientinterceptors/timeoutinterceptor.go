6844de5af3e7fd9362d64947a914f9f3a722012b
kevin <wanjunfeng@gmail.com>
2021-02-19 10:27:06 +0800

    prevent negative timeout settings

--- zrpc/internal/clientinterceptors/timeoutinterceptor.go
+++ zrpc/internal/clientinterceptors/timeoutinterceptor.go
@@ -11,9 +11,10 @@ import (
 func TimeoutInterceptor(timeout time.Duration) grpc.UnaryClientInterceptor {
 	return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn,
 		invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
-		if timeout == 0 {
+		if timeout <= 0 {
 			return invoker(ctx, method, req, reply, cc, opts...)
 		}
+
 		ctx, cancel := contextx.ShrinkDeadline(ctx, timeout)
 		defer cancel()
 		return invoker(ctx, method, req, reply, cc, opts...)
