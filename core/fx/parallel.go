f238290dd397ba0dbb229827bef5dc17df777fda
Kevin Wan <wanjunfeng@gmail.com>
2021-02-19 17:49:39 +0800

    fix golint issues in core/fx (#486)

--- core/fx/parallel.go
+++ core/fx/parallel.go
@@ -2,6 +2,7 @@ package fx
 
 import "github.com/tal-tech/go-zero/core/threading"
 
+// Parallel runs fns parallelly and waits for done.
 func Parallel(fns ...func()) {
 	group := threading.NewRoutineGroup()
 	for _, fn := range fns {
