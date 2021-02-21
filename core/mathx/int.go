b1d56cf44b639f7b754e013f6c51b20cf01a6dea
kevin <wanjunfeng@gmail.com>
2021-02-21 20:46:19 +0800

    fix golint issues in core/mathx

--- core/mathx/int.go
+++ core/mathx/int.go
@@ -1,5 +1,6 @@
 package mathx
 
+// MaxInt returns the larger one of a and b.
 func MaxInt(a, b int) int {
 	if a > b {
 		return a
@@ -8,6 +9,7 @@ func MaxInt(a, b int) int {
 	return b
 }
 
+// MinInt returns the smaller one of a and b.
 func MinInt(a, b int) int {
 	if a < b {
 		return a
