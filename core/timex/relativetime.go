9089cd1e8632c9d492c080c9b8f0a250f24da130
kevin <wanjunfeng@gmail.com>
2021-02-24 16:26:42 +0800

    fix golint issues in core/timex

--- core/timex/relativetime.go
+++ core/timex/relativetime.go
@@ -5,14 +5,18 @@ import "time"
 // Use the long enough past time as start time, in case timex.Now() - lastTime equals 0.
 var initTime = time.Now().AddDate(-1, -1, -1)
 
+// Now returns a relative time duration since initTime, which is not important.
+// The caller only needs to care about the relative value.
 func Now() time.Duration {
 	return time.Since(initTime)
 }
 
+// Since returns a diff since given d.
 func Since(d time.Duration) time.Duration {
 	return time.Since(initTime) - d
 }
 
+// Time returns current time, the same as time.Now().
 func Time() time.Time {
 	return initTime.Add(Now())
 }
