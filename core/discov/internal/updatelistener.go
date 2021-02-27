f02711a9cb39e5b7cc6e94e2b728b216c0c82182
Kevin Wan <wanjunfeng@gmail.com>
2021-02-27 23:56:18 +0800

    golint core/discov (#525)

--- core/discov/internal/updatelistener.go
+++ core/discov/internal/updatelistener.go
@@ -3,11 +3,13 @@
 package internal
 
 type (
+	// A KV is used to store an etcd entry with key and value.
 	KV struct {
 		Key string
 		Val string
 	}
 
+	// UpdateListener wraps the OnAdd and OnDelete methods.
 	UpdateListener interface {
 		OnAdd(kv KV)
 		OnDelete(kv KV)
