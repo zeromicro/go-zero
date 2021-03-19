c045d9c24383f0a01af7f0a28c97c29bdef4d768
kevin <wanjunfeng@gmail.com>
2021-03-19 18:36:10 +0800

    fix data race

--- zrpc/internal/serverinterceptors/timeoutinterceptor.go
+++ zrpc/internal/serverinterceptors/timeoutinterceptor.go
@@ -12,11 +12,12 @@ import (
 // UnaryTimeoutInterceptor returns a func that sets timeout to incoming unary requests.
 func UnaryTimeoutInterceptor(timeout time.Duration) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo,
-		handler grpc.UnaryHandler) (resp interface{}, err error) {
+		handler grpc.UnaryHandler) (interface{}, error) {
 		ctx, cancel := contextx.ShrinkDeadline(ctx, timeout)
 		defer cancel()
 
-		// guards resp and err
+		var resp interface{}
+		var err error
 		var lock sync.Mutex
 		done := make(chan struct{})
 		panicChan := make(chan interface{}, 1)
@@ -37,10 +38,9 @@ func UnaryTimeoutInterceptor(timeout time.Duration) grpc.UnaryServerInterceptor 
 		case p := <-panicChan:
 			panic(p)
 		case <-done:
-			// barrier cannot be removed, guards resp and err
 			lock.Lock()
 			defer lock.Unlock()
-			return
+			return resp, err
 		case <-ctx.Done():
 			return nil, ctx.Err()
 		}
