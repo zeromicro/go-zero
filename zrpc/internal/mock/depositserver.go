010c4102869c9b91d5d4c9b73a0a2aaa51fba1ca
kevin <wanjunfeng@gmail.com>
2021-03-28 20:02:26 +0800

    fix golint issues

--- zrpc/internal/mock/depositserver.go
+++ zrpc/internal/mock/depositserver.go
@@ -7,9 +7,11 @@ import (
 	"google.golang.org/grpc/status"
 )
 
+// DespositServer is used for mocking.
 type DepositServer struct {
 }
 
+// Deposit handles the deposit requests.
 func (*DepositServer) Deposit(ctx context.Context, req *DepositRequest) (*DepositResponse, error) {
 	if req.GetAmount() < 0 {
 		return nil, status.Errorf(codes.InvalidArgument, "cannot deposit %v", req.GetAmount())
