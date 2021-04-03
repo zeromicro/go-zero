4fa9fdd7bf54c3de05b341b5a749b886cb8703ae
Oraoto <oraoto@outlook.com>
2021-04-03 21:02:17 +0800

    Simplify contextx.ShrinkDeadline

--- core/contextx/deadline.go
+++ core/contextx/deadline.go
@@ -8,12 +8,5 @@ import (
 // ShrinkDeadline returns a new Context with proper deadline base on the given ctx and timeout.
 // And returns a cancel function as well.
 func ShrinkDeadline(ctx context.Context, timeout time.Duration) (context.Context, func()) {
-	if deadline, ok := ctx.Deadline(); ok {
-		leftTime := time.Until(deadline)
-		if leftTime < timeout {
-			timeout = leftTime
-		}
-	}
-
-	return context.WithDeadline(ctx, time.Now().Add(timeout))
+	return context.WithTimeout(ctx, timeout)
 }
