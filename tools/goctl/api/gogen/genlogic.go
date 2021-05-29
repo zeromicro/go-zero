546fcd8bab3257edf5c7147d7853538ccc25e804
kingxt <kingxt4job@gmail.com>
2021-05-29 23:01:02 +0800

    fix #736 (#738)
    * optimize performance
    
    * rename
    
    * rename
    
    * revert

--- tools/goctl/api/gogen/genlogic.go
+++ tools/goctl/api/gogen/genlogic.go
@@ -39,10 +39,10 @@ func (l *{{.logic}}) {{.function}}({{.request}}) {{.responseType}} {
 }
 `
 
-func genLogic(dir string, cfg *config.Config, api *spec.ApiSpec) error {
+func genLogic(dir, rootPkg string, cfg *config.Config, api *spec.ApiSpec) error {
 	for _, g := range api.Service.Groups {
 		for _, r := range g.Routes {
-			err := genLogicByRoute(dir, cfg, g, r)
+			err := genLogicByRoute(dir, rootPkg, cfg, g, r)
 			if err != nil {
 				return err
 			}
@@ -51,19 +51,14 @@ func genLogic(dir string, cfg *config.Config, api *spec.ApiSpec) error {
 	return nil
 }
 
-func genLogicByRoute(dir string, cfg *config.Config, group spec.Group, route spec.Route) error {
+func genLogicByRoute(dir, rootPkg string, cfg *config.Config, group spec.Group, route spec.Route) error {
 	logic := getLogicName(route)
 	goFile, err := format.FileNamingFormat(cfg.NamingFormat, logic)
 	if err != nil {
 		return err
 	}
 
-	parentPkg, err := getParentPackage(dir)
-	if err != nil {
-		return err
-	}
-
-	imports := genLogicImports(route, parentPkg)
+	imports := genLogicImports(route, rootPkg)
 	var responseString string
 	var returnString string
 	var requestString string
