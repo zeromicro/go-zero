6f6a6d357e022b91bba8ba266de9bf630c337cab
zjbztianya <spcjv5@gmail.com>
2021-05-29 17:11:09 +0800

    Subset selection algorithm: use Google’s solution deterministic subsetting replace random subsetting

--- zrpc/internal/resolver/subset.go
+++ zrpc/internal/resolver/subset.go
@@ -1,14 +1,29 @@
 package resolver
 
-import "math/rand"
+import (
+	"github.com/tal-tech/go-zero/core/hash"
+	"github.com/tal-tech/go-zero/core/sysx"
+	"math/rand"
+)
 
 func subset(set []string, sub int) []string {
-	rand.Shuffle(len(set), func(i, j int) {
-		set[i], set[j] = set[j], set[i]
-	})
 	if len(set) <= sub {
+		rand.Shuffle(len(set), func(i, j int) {
+			set[i], set[j] = set[j], set[i]
+		})
 		return set
 	}
 
-	return set[:sub]
+	// group clients into rounds, each round uses the same shuffled list
+	count := uint64(len(set) / sub)
+	clientID := hash.Hash([]byte(sysx.Hostname()))
+	round := clientID / count
+
+	r := rand.New(rand.NewSource(int64(round)))
+	r.Shuffle(len(set), func(i, j int) {
+		set[i], set[j] = set[j], set[i]
+	})
+
+	start := (clientID % count) * uint64(sub)
+	return set[start : start+uint64(sub)]
 }
