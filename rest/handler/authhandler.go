e64b581fe2d687510e15b4d0ecc803f4fe4787ad
kevin <wanjunfeng@gmail.com>
2021-08-21 23:02:28 +0800

    make sure setting code happen before callback in rest

--- rest/handler/authhandler.go
+++ rest/handler/authhandler.go
@@ -112,11 +112,12 @@ func unauthorized(w http.ResponseWriter, r *http.Request, err error, callback Un
 	} else {
 		detailAuthLog(r, noDetailReason)
 	}
+
+	writer.WriteHeader(http.StatusUnauthorized)
+
 	if callback != nil {
 		callback(writer, r, err)
 	}
-
-	writer.WriteHeader(http.StatusUnauthorized)
 }
 
 type guardedResponseWriter struct {
