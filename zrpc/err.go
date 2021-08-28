5d6e0c4016a0b13a985becebe25fe693cbb53cdf
Chen Quan <chenquan.dev@foxmail.com>
2021-08-28 10:41:51 +0800

    Delete customized breaker errors

--- zrpc/err.go
+++
@@ -1,6 +0,0 @@
-package zrpc
-
-import "github.com/tal-tech/go-zero/zrpc/internal/codes"
-
-// AddBreakerErrors add customized breaker errors for the client and server.
-var AddBreakerErrors = codes.AddBreakerErrors
--- zrpc/internal/codes/accept.go
+++ zrpc/internal/codes/accept.go
@@ -1,41 +1,16 @@
 package codes
 
 import (
-	"github.com/tal-tech/go-zero/core/lang"
-	"sync"
-
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
-
-// breakerErrors storage rejected errors.
-var breakerErrors = make(map[error]lang.PlaceholderType)
-
-// once only allow breakerErrors to be added once.
-var once sync.Once
-
-// AddBreakerErrors add customized breaker errors for the client and server.
-func AddBreakerErrors(errs ...error) {
-	once.Do(func() {
-		for _, err := range errs {
-			breakerErrors[err] = lang.Placeholder
-		}
-	})
-}
 
 // Acceptable checks if given error is acceptable.
 func Acceptable(err error) bool {
 	switch status.Code(err) {
 	case codes.DeadlineExceeded, codes.Internal, codes.Unavailable, codes.DataLoss:
 		return false
-	case codes.Unknown:
-		return acceptableUnknown(err)
 	default:
 		return true
 	}
-}
-
-func acceptableUnknown(err error) bool {
-	_, ok := breakerErrors[err]
-	return !ok
 }
