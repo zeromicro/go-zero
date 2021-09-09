8ee67b6f9c10007c308ba6f9f5f3265dd2d92d9d
anqiansong <anqiansong@bytedance.com>
2021-09-09 21:23:00 +0800

    remove unused code

--- tools/goctl/rpc/generator/genpb.go
+++ tools/goctl/rpc/generator/genpb.go
@@ -15,8 +15,6 @@ import (
 	"github.com/tal-tech/go-zero/tools/goctl/vars"
 )
 
-const googleProtocGenGoErr = `--go_out: protoc-gen-go: plugins are not supported; use 'protoc --go-grpc_out=...' to generate gRPC`
-
 // GenPb generates the pb.go file, which is a layer of packaging for protoc to generate gprc,
 // but the commands and flags in protoc are not completely joined in goctl. At present, proto_path(-I) is introduced
 func (g *DefaultGenerator) GenPb(ctx DirContext, protoImportPath []string, proto parser.Proto, _ *conf.Config, goOptions ...string) error {
