6299a5ec98129ffee42e981c900b42f9d6aa4a6c
anqiansong <anqiansong@bytedance.com>
2021-10-21 22:11:26 +0800

    Update build.sh

--- tools/goctl/api/parser/g4/build.sh
+++ tools/goctl/api/parser/g4/build.sh
@@ -2,23 +2,22 @@
 
 target="antlr-4.9-complete.jar"
 antlr="${PWD}/$target"
-echo "检测 java 环境"
+echo "check env JAVA"
 javaPath=$(which java)
 
 echo "$antlr"
 if [ ! -f "$javaPath" ]; then
-  echo "java 不存在"
+  echo "JAVA is not install"
   exit 1
 fi
 
-echo "java 环境已支持"
-echo "检测 antlr 是否存在"
+echo "JAVA installed"
+echo "check library ANTLR"
 if [ ! -f "$antlr" ];then
-  echo "antlr 不存在，开始下载..."
+  echo "ANTLR not exists, start to download ..."
   curl -O https://www.antlr.org/download/${target}
 fi
 
-#export CLASSPATH="/Users/bytedance/keson/workspace/go-zero/tools/goctl/api/parser/g4/antlr-4.9-complete.jar:$CLASSPATH"
 export CLASSPATH=".:$antlr:$CLASSPATH"
 alias antlr4='java -Xmx500M -cp $antlr:$CLASSPATH org.antlr.v4.Tool'
-antlr4 -o ./gen/api -package api -visitor ImportParser.g4  -Dlanguage=Go -no-listener -lib . ./ApiParser.g4
+antlr4 -o ./gen/api -package api -visitor  -Dlanguage=Go -no-listener -lib . ./ApiParser.g4
