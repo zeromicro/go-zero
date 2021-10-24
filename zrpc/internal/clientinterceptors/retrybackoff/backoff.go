bd4dc5e7c12d3f898c7a5567aac110f8f9aa66a1
Chen Quan <chenquan.dev@foxmail.com>
2021-10-24 23:43:15 +0800

    Fix a bug

--- zrpc/internal/clientinterceptors/retrybackoff/backoff.go
+++ zrpc/internal/clientinterceptors/retrybackoff/backoff.go
@@ -10,7 +10,7 @@ type BackoffFunc func(ctx context.Context, attempt int) time.Duration
 
 func BackoffLinearWithJitter(waitBetween time.Duration, jitterFraction float64) BackoffFunc {
 	rand.Float64()
-	r := rand.New(rand.NewSource(time.Now().UnixMilli()))
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	return func(ctx context.Context, attempt int) time.Duration {
 		multiplier := jitterFraction * (r.Float64()*2 - 1)
 		return time.Duration(float64(waitBetween) * (1 + multiplier))
