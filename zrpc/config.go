5d5cc938498c6fb86337fac77595032a03d3eddc
kevin <wanjunfeng@gmail.com>
2021-10-27 20:38:32 +0800

    feat: change MaxRetries default to 0, disable retry

--- zrpc/config.go
+++ zrpc/config.go
@@ -18,7 +18,7 @@ type (
 		// setting 0 means no timeout
 		Timeout      int64 `json:",default=2000"`
 		CpuThreshold int64 `json:",default=900,range=[0:1000]"`
-		MaxRetries   int   `json:",range=[0:]"`
+		MaxRetries   int   `json:",default=0,range=[0:]"`
 	}
 
 	// A RpcClientConf is a rpc client config.
