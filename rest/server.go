ff85c7d2c79f09478327c0b47439cbe5f7f3efe3
kevin <wanjunfeng@gmail.com>
2021-11-01 08:40:42 +0800

    feat: slow threshold customizable in rest

--- rest/server.go
+++ rest/server.go
@@ -11,6 +11,9 @@ import (
 	"github.com/tal-tech/go-zero/rest/router"
 )
 
+// SetSlowThreshold sets the slow threshold.
+var SetSlowThreshold = handler.SetSlowThreshold
+
 type (
 	runOptions struct {
 		start func(*engine) error
