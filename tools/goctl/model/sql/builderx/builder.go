8338cec37e1a5d9286c593e24529519be1131394
anqiansong <anqiansong@bytedance.com>
2021-11-07 22:03:31 +0800

    Remove unused code

--- tools/goctl/model/sql/builderx/builder.go
+++ tools/goctl/model/sql/builderx/builder.go
@@ -4,21 +4,6 @@ import (
 	"github.com/tal-tech/go-zero/core/stores/builderx"
 )
 
-// Deprecated: Use github.com/tal-tech/go-zero/core/stores/builderx.NewEq instead.
-func NewEq(in interface{}) builderx.Eq {
-	return builderx.NewEq(in)
-}
-
-// Deprecated: Use github.com/tal-tech/go-zero/core/stores/builderx.NewGt instead.
-func NewGt(in interface{}) builderx.Gt {
-	return builderx.NewGt(ToMap(in))
-}
-
-// Deprecated: Use github.com/tal-tech/go-zero/core/stores/builderx.ToMap instead.
-func ToMap(in interface{}) map[string]interface{} {
-	return builderx.ToMap(in)
-}
-
 // Deprecated: Use github.com/tal-tech/go-zero/core/stores/builderx.FieldNames instead.
 func FieldNames(in interface{}) []string {
 	return builderx.FieldNames(in)
