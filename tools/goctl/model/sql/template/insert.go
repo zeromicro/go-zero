2398c1763c274ed0c8cc02fbd5e91ca4baa87a88
anqiansong <anqiansong@bytedance.com>
2021-11-07 21:35:46 +0800

    fix #1205

--- tools/goctl/model/sql/template/insert.go
+++ tools/goctl/model/sql/template/insert.go
@@ -2,7 +2,7 @@ package template
 
 // Insert defines a template for insert code in model
 var Insert = `
-func (m *default{{.upperStartCamelObject}}Model) Insert(data {{.upperStartCamelObject}}) (sql.Result,error) {
+func (m *default{{.upperStartCamelObject}}Model) Insert(data *{{.upperStartCamelObject}}) (sql.Result,error) {
 	{{if .withCache}}{{if .containsIndexCache}}{{.keys}}
     ret, err := m.Exec(func(conn sqlx.SqlConn) (result sql.Result, err error) {
 		query := fmt.Sprintf("insert into %s (%s) values ({{.expression}})", m.table, {{.lowerStartCamelObject}}RowsExpectAutoSet)
@@ -15,5 +15,5 @@ func (m *default{{.upperStartCamelObject}}Model) Insert(data {{.upperStartCamelO
 }
 `
 
-// InsertMethod defines a interface method template for insert code in model
-var InsertMethod = `Insert(data {{.upperStartCamelObject}}) (sql.Result,error)`
+// InsertMethod defines an interface method template for insert code in model
+var InsertMethod = `Insert(data *{{.upperStartCamelObject}}) (sql.Result,error)`
