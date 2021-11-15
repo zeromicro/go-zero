37b12a30d7bf83ad623b65844915ee6f6ea498a7
kevin <wanjunfeng@gmail.com>
2021-11-15 23:12:46 +0800

    Revert "feat: enable retry for zrpc (#1237)"
    This reverts commit 09eb53f30819f14b434f6459077f74dadb35b4fc.

--- zrpc/config.go
+++ zrpc/config.go
@@ -18,7 +18,8 @@ type (
 		// setting 0 means no timeout
 		Timeout      int64 `json:",default=2000"`
 		CpuThreshold int64 `json:",default=900,range=[0:1000]"`
-		MaxRetries   int   `json:",default=0,range=[0:]"`
+		// TODO: enable it in v1.2.4
+		// MaxRetries   int   `json:",default=0,range=[0:]"`
 	}
 
 	// A RpcClientConf is a rpc client config.
@@ -29,8 +30,9 @@ type (
 		App       string          `json:",optional"`
 		Token     string          `json:",optional"`
 		NonBlock  bool            `json:",optional"`
-		Retry     bool            `json:",optional"` // grpc auto retry
-		Timeout   int64           `json:",default=2000"`
+		// TODO: enable it in v1.2.4
+		// Retry     bool            `json:",optional"` // grpc auto retry
+		Timeout int64 `json:",default=2000"`
 	}
 )
 
