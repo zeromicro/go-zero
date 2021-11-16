f8f88e4e186eadc8e7456e862ff702bbc1f2a3c7
kevin <wanjunfeng@gmail.com>
2021-11-16 21:17:25 +0800

    chore: update goctl version

--- tools/goctl/internal/version/version.go
+++ tools/goctl/internal/version/version.go
@@ -6,7 +6,7 @@ import (
 )
 
 // BuildVersion is the version of goctl.
-const BuildVersion = "1.2.3"
+const BuildVersion = "1.2.3-cli"
 
 // GetGoctlVersion returns BuildVersion
 func GetGoctlVersion() string {
