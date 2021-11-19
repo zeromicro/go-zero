87c038c7ebc3aae46231cde087bbce5cd1a6ff7c
anqiansong <anqiansong@bytedance.com>
2021-11-19 18:41:47 +0800

    remove head desc

--- tools/goctl/api/parser/g4/gen/api/apiparser_visitor.go
+++ tools/goctl/api/parser/g4/gen/api/apiparser_visitor.go
@@ -1,5 +1,3 @@
-// Code generated from /Users/bytedance/keson/workspace/go-zero/tools/goctl/api/parser/g4/ApiParser.g4 by ANTLR 4.9.1. DO NOT EDIT.
-
 package api // ApiParser
 import "github.com/zeromicro/antlr"
 
