f20c39f74ba16eeb17a47a3c717011b8a78714c2
kevin <wanjunfeng@gmail.com>
2021-12-18 22:55:18 +0800

    chore: add tests & refactor

--- core/stores/sqlc/cachedsql.go
+++ core/stores/sqlc/cachedsql.go
@@ -2,7 +2,6 @@ package sqlc
 
 import (
 	"database/sql"
-	"log"
 	"time"
 
 	"github.com/tal-tech/go-zero/core/stores/cache"
@@ -40,14 +39,6 @@ type (
 	}
 )
 
-// NewNodeConn returns a CachedConn with a redis node cache.
-func NewNodeConn(db sqlx.SqlConn, rds *redis.Redis, opts ...cache.Option) CachedConn {
-	return CachedConn{
-		db:    db,
-		cache: cache.NewNode(rds, exclusiveCalls, stats, sql.ErrNoRows, opts...),
-	}
-}
-
 // NewConn returns a CachedConn with a redis cluster cache.
 func NewConn(db sqlx.SqlConn, c cache.CacheConf, opts ...cache.Option) CachedConn {
 	return CachedConn{
@@ -58,12 +49,17 @@ func NewConn(db sqlx.SqlConn, c cache.CacheConf, opts ...cache.Option) CachedCon
 
 // NewConnWithCache returns a CachedConn with a custom cache.
 func NewConnWithCache(db sqlx.SqlConn, c cache.Cache) CachedConn {
-	if c == nil {
-		log.Fatal("Invalid cache component")
-	}
 	return CachedConn{
 		db:    db,
 		cache: c,
+	}
+}
+
+// NewNodeConn returns a CachedConn with a redis node cache.
+func NewNodeConn(db sqlx.SqlConn, rds *redis.Redis, opts ...cache.Option) CachedConn {
+	return CachedConn{
+		db:    db,
+		cache: cache.NewNode(rds, exclusiveCalls, stats, sql.ErrNoRows, opts...),
 	}
 }
 
