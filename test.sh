aedd4eb6db7528a669894b0d2d6925478c8b3076
chenli <1920853199@qq.com>
2022-01-05 18:35:58 +0800

    add test.sh

---
+++ test.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+starttest() {
+	set -e
+	GO111MODULE=on go test -race ./...
+}
+
+if [ -z "${TEST_MOD}" ]; then
+	docker run --rm --name go-zero -ti \
+  		-v `pwd`:/go/src/github.com/zeromicro/go-zero \
+  		--workdir /go/src/github.com/zeromicro/go-zero \
+        --env GOPROXY=https://goproxy.cn\
+		--env TEST_MOD=local \
+  		golang:1.16 \
+  		sh test.sh
+else
+	starttest
+fi
+
