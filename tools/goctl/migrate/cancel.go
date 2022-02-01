8f46eab97739f8a81dd3b8fb33e184c90b707b3e
Kevin Wan <wanjunfeng@gmail.com>
2022-02-01 13:58:08 +0800

    fix: goctl not compile on windows (#1500)

---
+++ tools/goctl/migrate/cancel.go
@@ -0,0 +1,31 @@
+//go:build linux || darwin
+// +build linux darwin
+
+package migrate
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/zeromicro/go-zero/core/syncx"
+	"github.com/zeromicro/go-zero/tools/goctl/util/console"
+)
+
+func cancelOnSignals() {
+	doneChan := syncx.NewDoneChan()
+	defer doneChan.Close()
+
+	go func(dc *syncx.DoneChan) {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGTERM, syscall.SIGKILL, syscall.SIGINT, syscall.SIGTSTP, syscall.SIGQUIT)
+		select {
+		case <-c:
+			console.Error(`
+migrate failed, reason: "User Canceled"`)
+			os.Exit(0)
+		case <-dc.Done():
+			return
+		}
+	}(doneChan)
+}
