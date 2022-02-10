69e882ec3feb8c642585be809650f8436027a9d7
chenquan <chenquan.dev@foxmail.com>
2022-02-10 08:21:19 +0800

    chore(tracing): optimize the generation of spanId and traceId
    Signed-off-by: chenquan <chenquan.dev@foxmail.com>

--- core/logx/tracelogger.go
+++ core/logx/tracelogger.go
@@ -77,12 +77,9 @@ func (l *traceLogger) WithDuration(duration time.Duration) Logger {
 }
 
 func (l *traceLogger) write(writer io.Writer, level string, val interface{}) {
-	traceID := traceIdFromContext(l.ctx)
-	spanID := spanIdFromContext(l.ctx)
-
 	switch encoding {
 	case plainEncodingType:
-		writePlainAny(writer, level, val, l.Duration, traceID, spanID)
+		writePlainAny(writer, level, val, l.Duration, l.Trace, l.Span)
 	default:
 		outputJson(writer, &traceLogger{
 			logEntry: logEntry{
@@ -91,8 +88,8 @@ func (l *traceLogger) write(writer io.Writer, level string, val interface{}) {
 				Duration:  l.Duration,
 				Content:   val,
 			},
-			Trace: traceID,
-			Span:  spanID,
+			Trace: l.Trace,
+			Span:  l.Span,
 		})
 	}
 }
@@ -100,7 +97,9 @@ func (l *traceLogger) write(writer io.Writer, level string, val interface{}) {
 // WithContext sets ctx to log, for keeping tracing information.
 func WithContext(ctx context.Context) Logger {
 	return &traceLogger{
-		ctx: ctx,
+		ctx:   ctx,
+		Trace: traceIdFromContext(ctx),
+		Span:  spanIdFromContext(ctx),
 	}
 }
 
