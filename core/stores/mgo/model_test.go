a800170df8df2a160c53283bf696a63c1116c493
kevin <wanjunfeng@gmail.com>
2022-02-25 16:54:37 +0800

    wip: backup

---
+++ core/stores/mgo/model_test.go
@@ -0,0 +1,14 @@
+package mgo
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWithTimeout(t *testing.T) {
+	o := defaultOptions()
+	WithTimeout(time.Second)(o)
+	assert.Equal(t, time.Second, o.timeout)
+}
---
+++ core/stores/mgo/util.go
@@ -0,0 +1,10 @@
+package mgo
+
+import "strings"
+
+const mongoAddrSep = ","
+
+// FormatAddr formats mongo hosts to a string.
+func FormatAddr(hosts []string) string {
+	return strings.Join(hosts, mongoAddrSep)
+}
