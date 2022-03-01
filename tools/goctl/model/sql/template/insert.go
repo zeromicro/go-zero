cc4c4928e006ac1bcd5477067c2aa7b75bba913b
toutou_o <33993460+kurimi1@users.noreply.github.com>
2022-03-01 19:53:23 +0800

    🐞 fix(gen): pg gen of insert (#1591)
    Co-authored-by: kurimi1 <d0n41df@gmail.com>

--- tools/goctl/model/sql/template/insert.go
+++ tools/goctl/model/sql/template/insert.go
@@ -15,5 +15,24 @@ func (m *default{{.upperStartCamelObject}}Model) Insert(ctx context.Context, dat
 }
 `
 
+// pg
+var InsertPg = `
+func (m *default{{.upperStartCamelObject}}Model) Insert(data *{{.upperStartCamelObject}}) (int64,error) {
+	{{if .withCache}}{{if .containsIndexCache}}{{.keys}}
+    ret, err := m.Exec(func(conn sqlx.SqlConn) (result sql.Result, err error) {
+		query := fmt.Sprintf("insert into %s (%s) values ({{.expression}})", m.table, {{.lowerStartCamelObject}}RowsExpectAutoSet)
+		return conn.Exec(query, {{.expressionValues}})
+	}, {{.keyValues}}){{else}}query := fmt.Sprintf("insert into %s (%s) values ({{.expression}})", m.table, {{.lowerStartCamelObject}}RowsExpectAutoSet)
+    ret,err:=m.ExecNoCache(query, {{.expressionValues}})
+	{{end}}{{else}}query := fmt.Sprintf("insert into %s (%s) values ({{.expression}}) RETURNING id", m.table, {{.lowerStartCamelObject}}RowsExpectAutoSet)
+	var id int64
+    err:=m.conn.QueryRow(&id, query, {{.expressionValues}}){{end}}
+	return id,err
+}
+`
+
 // InsertMethod defines an interface method template for insert code in model
 var InsertMethod = `Insert(ctx context.Context, data *{{.upperStartCamelObject}}) (sql.Result,error)`
+
+// pg
+var InsertMethodPg = `Insert(ctx context.Context, data *{{.upperStartCamelObject}}) (int64,error)`
