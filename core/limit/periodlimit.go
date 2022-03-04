96c128c58a338453375e49c47a0d0b91fb9b3a0c
qi <51255100+imzhongqi@users.noreply.github.com>
2022-03-04 16:14:45 +0800

    fix: HitQuota should be returned instead of Allowed when limit is equal to 1. (#1581)

--- core/limit/periodlimit.go
+++ core/limit/periodlimit.go
@@ -14,8 +14,8 @@ local window = tonumber(ARGV[2])
 local current = redis.call("INCRBY", KEYS[1], 1)
 if current == 1 then
     redis.call("expire", KEYS[1], window)
-    return 1
-elseif current < limit then
+end
+if current < limit then
     return 1
 elseif current == limit then
     return 2
