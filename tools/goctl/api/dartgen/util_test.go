b277720f9abdea007e54ac4671956ece4f0338c0
fyn <fyn.xiu@gmail.com>
2022-03-07 12:21:29 +0800

    fix(goctl): dart gen user defined struct array

--- tools/goctl/api/dartgen/util_test.go
+++ tools/goctl/api/dartgen/util_test.go
@@ -37,3 +37,35 @@ func Test_getPropertyFromMember(t *testing.T) {
 		})
 	}
 }
+
+func Test_specTypeToDart(t *testing.T) {
+	tests := []struct {
+		name     string
+		specType spec.Type
+		want     string
+		wantErr  bool
+	}{
+		{
+			name:     "[]string should return List<String>",
+			specType: spec.ArrayType{RawName: "[]string", Value: spec.PrimitiveType{RawName: "string"}},
+			want:     "List<String>",
+		},
+		{
+			name:     "[]Foo should return List<Foo>",
+			specType: spec.ArrayType{RawName: "[]Foo", Value: spec.DefineStruct{RawName: "Foo"}},
+			want:     "List<Foo>",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := specTypeToDart(tt.specType)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("specTypeToDart() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("specTypeToDart() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
