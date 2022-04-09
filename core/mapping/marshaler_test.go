36c6d7030c644f8de442bcc5bb770a3af96c5842
kevin <wanjunfeng@gmail.com>
2022-04-09 23:20:41 +0800

    add more tests

--- core/mapping/marshaler_test.go
+++ core/mapping/marshaler_test.go
@@ -94,6 +94,29 @@ func TestMarshal_Nested(t *testing.T) {
 	assert.Equal(t, "Shanghai", m["json"]["address"].(address).City)
 }
 
+func TestMarshal_NestedPtr(t *testing.T) {
+	type address struct {
+		Country string `json:"country"`
+		City    string `json:"city"`
+	}
+	v := struct {
+		Name    string   `json:"name,options=[kevin,wan]"`
+		Address *address `json:"address"`
+	}{
+		Name: "kevin",
+		Address: &address{
+			Country: "China",
+			City:    "Shanghai",
+		},
+	}
+
+	m, err := Marshal(v)
+	assert.Nil(t, err)
+	assert.Equal(t, "kevin", m["json"]["name"])
+	assert.Equal(t, "China", m["json"]["address"].(*address).Country)
+	assert.Equal(t, "Shanghai", m["json"]["address"].(*address).City)
+}
+
 func TestMarshal_Slice(t *testing.T) {
 	v := struct {
 		Name []string `json:"name"`
