f6c5cfa0edfc70669da06f9e2f7ae4240aa084c3
kevin <wanjunfeng@gmail.com>
2022-04-15 21:13:04 +0800

    fix wrong dependency

--- core/stores/mongo/sessionmanager.go
+++ core/stores/mongo/sessionmanager.go
@@ -1,14 +1,12 @@
 package mongo
 
 import (
-	"context"
 	"io"
 	"time"
 
+	"github.com/globalsign/mgo"
 	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/core/syncx"
-	"go.mongodb.org/mongo-driver/mongo"
-	mopt "go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const (
@@ -19,24 +17,25 @@ const (
 var sessionManager = syncx.NewResourceManager()
 
 type concurrentSession struct {
-	*mongo.Client
+	*mgo.Session
 	limit syncx.TimeoutLimit
 }
 
 func (cs *concurrentSession) Close() error {
-	return cs.Client.Disconnect(context.Background())
+	cs.Session.Close()
+	return nil
 }
 
 func getConcurrentSession(url string) (*concurrentSession, error) {
 	val, err := sessionManager.GetResource(url, func() (io.Closer, error) {
-		cli, err := mongo.Connect(context.Background(), mopt.Client().ApplyURI(url))
+		mgoSession, err := mgo.Dial(url)
 		if err != nil {
 			return nil, err
 		}
 
 		concurrentSess := &concurrentSession{
-			Client: cli,
-			limit:  syncx.NewTimeoutLimit(defaultConcurrency),
+			Session: mgoSession,
+			limit:   syncx.NewTimeoutLimit(defaultConcurrency),
 		}
 
 		return concurrentSess, nil
@@ -48,16 +47,16 @@ func getConcurrentSession(url string) (*concurrentSession, error) {
 	return val.(*concurrentSession), nil
 }
 
-func (cs *concurrentSession) putSession(session mongo.Session) {
+func (cs *concurrentSession) putSession(session *mgo.Session) {
 	if err := cs.limit.Return(); err != nil {
 		logx.Error(err)
 	}
 
 	// anyway, we need to close the session
-	session.EndSession(context.Background())
+	session.Close()
 }
 
-func (cs *concurrentSession) takeSession(opts ...Option) (mongo.Session, error) {
+func (cs *concurrentSession) takeSession(opts ...Option) (*mgo.Session, error) {
 	o := defaultOptions()
 	for _, opt := range opts {
 		opt(o)
@@ -67,5 +66,5 @@ func (cs *concurrentSession) takeSession(opts ...Option) (mongo.Session, error) 
 		return nil, err
 	}
 
-	return cs.Client.StartSession()
+	return cs.Copy(), nil
 }
