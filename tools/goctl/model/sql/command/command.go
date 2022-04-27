a19a2cb86ea87539437c7b0cc2c301510006b1df
anqiansong <anqiansong@bytedance.com>
2022-04-27 21:50:13 +0800

    Format code

--- tools/goctl/model/sql/command/command.go
+++ tools/goctl/model/sql/command/command.go
@@ -97,9 +97,9 @@ func MySqlDataSource(ctx *cli.Context) error {
 	return fromMysqlDataSource(url, dir, patterns, cfg, cache, idea)
 }
 
-type patterns map[string]struct{}
+type pattern map[string]struct{}
 
-func (p patterns) Match(s string) bool {
+func (p pattern) Match(s string) bool {
 	for v := range p {
 		match, err := filepath.Match(v, s)
 		if err != nil {
@@ -113,7 +113,7 @@ func (p patterns) Match(s string) bool {
 	return false
 }
 
-func (p patterns) list() []string {
+func (p pattern) list() []string {
 	var ret []string
 	for v := range p {
 		ret = append(ret, v)
@@ -121,8 +121,8 @@ func (p patterns) list() []string {
 	return ret
 }
 
-func parseTableList(tableValue []string) patterns {
-	tablePattern := make(patterns)
+func parseTableList(tableValue []string) pattern {
+	tablePattern := make(pattern)
 	for _, v := range tableValue {
 		fields := strings.FieldsFunc(v, func(r rune) bool {
 			return r == ','
@@ -200,14 +200,14 @@ func fromDDL(src, dir string, cfg *config.Config, cache, idea bool, database str
 	return nil
 }
 
-func fromMysqlDataSource(url, dir string, patterns patterns, cfg *config.Config, cache, idea bool) error {
+func fromMysqlDataSource(url, dir string, tablePat pattern, cfg *config.Config, cache, idea bool) error {
 	log := console.NewConsole(idea)
 	if len(url) == 0 {
 		log.Error("%v", "expected data source of mysql, but nothing found")
 		return nil
 	}
 
-	if len(patterns) == 0 {
+	if len(tablePat) == 0 {
 		log.Error("%v", "expected table or table globbing patterns, but nothing found")
 		return nil
 	}
@@ -229,7 +229,7 @@ func fromMysqlDataSource(url, dir string, patterns patterns, cfg *config.Config,
 
 	matchTables := make(map[string]*model.Table)
 	for _, item := range tables {
-		if !patterns.Match(item) {
+		if !tablePat.Match(item) {
 			continue
 		}
 
