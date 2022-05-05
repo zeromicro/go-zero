2e11996009177966a87addd413196cd8b27b5aff
kevin <wanjunfeng@gmail.com>
2022-05-05 23:09:58 +0800

    fix #1866

--- core/logx/logs.go
+++ core/logx/logs.go
@@ -113,8 +113,36 @@ func Errorw(msg string, fields ...LogField) {
 // Field returns a LogField for the given key and value.
 func Field(key string, value interface{}) LogField {
 	switch val := value.(type) {
+	case error:
+		return LogField{Key: key, Value: val.Error()}
+	case []error:
+		var errs []string
+		for _, err := range val {
+			errs = append(errs, err.Error())
+		}
+		return LogField{Key: key, Value: errs}
 	case time.Duration:
 		return LogField{Key: key, Value: fmt.Sprint(val)}
+	case []time.Duration:
+		var durs []string
+		for _, dur := range val {
+			durs = append(durs, fmt.Sprint(dur))
+		}
+		return LogField{Key: key, Value: durs}
+	case []time.Time:
+		var times []string
+		for _, t := range val {
+			times = append(times, fmt.Sprint(t))
+		}
+		return LogField{Key: key, Value: times}
+	case fmt.Stringer:
+		return LogField{Key: key, Value: val.String()}
+	case []fmt.Stringer:
+		var strs []string
+		for _, str := range val {
+			strs = append(strs, str.String())
+		}
+		return LogField{Key: key, Value: strs}
 	default:
 		return LogField{Key: key, Value: val}
 	}
