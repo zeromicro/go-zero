c84446aca583931c28b66d4801638189eabc1cb9
anqiansong <anqiansong@gmail.com>
2022-05-22 21:43:08 +0800

    Format code

--- tools/goctl/model/sql/gen/update.go
+++ tools/goctl/model/sql/gen/update.go
@@ -55,58 +55,50 @@ func genUpdate(table Table, withCache, postgreSql bool) (
 		)
 	}
 	camelTableName := table.Name.ToCamel()
-	text, err := pathx.LoadTemplate(
-		category, updateTemplateFile, template.Update,
-	)
+	text, err := pathx.LoadTemplate(category, updateTemplateFile, template.Update)
 	if err != nil {
 		return "", "", err
 	}
 
-	output, err := util.With("update").
-		Parse(text).
-		Execute(
-			map[string]interface{}{
-				"withCache":             withCache,
-				"containsIndexCache":    table.ContainsUniqueCacheKey,
-				"upperStartCamelObject": camelTableName,
-				"keys":                  strings.Join(keys, "\n"),
-				"keyValues":             strings.Join(keyVars, ", "),
-				"primaryCacheKey":       table.PrimaryCacheKey.DataKeyExpression,
-				"primaryKeyVariable":    table.PrimaryCacheKey.KeyLeft,
-				"lowerStartCamelObject": stringx.From(camelTableName).Untitle(),
-				"upperStartCamelPrimaryKey": util.EscapeGolangKeyword(
-					stringx.From(table.PrimaryKey.Name.ToCamel()).Title(),
-				),
-				"originalPrimaryKey": wrapWithRawString(
-					table.PrimaryKey.Name.Source(), postgreSql,
-				),
-				"expressionValues": strings.Join(
-					expressionValues, ", ",
-				),
-				"postgreSql": postgreSql,
-				"data":       table,
-			},
-		)
+	output, err := util.With("update").Parse(text).Execute(
+		map[string]interface{}{
+			"withCache":             withCache,
+			"containsIndexCache":    table.ContainsUniqueCacheKey,
+			"upperStartCamelObject": camelTableName,
+			"keys":                  strings.Join(keys, "\n"),
+			"keyValues":             strings.Join(keyVars, ", "),
+			"primaryCacheKey":       table.PrimaryCacheKey.DataKeyExpression,
+			"primaryKeyVariable":    table.PrimaryCacheKey.KeyLeft,
+			"lowerStartCamelObject": stringx.From(camelTableName).Untitle(),
+			"upperStartCamelPrimaryKey": util.EscapeGolangKeyword(
+				stringx.From(table.PrimaryKey.Name.ToCamel()).Title(),
+			),
+			"originalPrimaryKey": wrapWithRawString(
+				table.PrimaryKey.Name.Source(), postgreSql,
+			),
+			"expressionValues": strings.Join(
+				expressionValues, ", ",
+			),
+			"postgreSql": postgreSql,
+			"data":       table,
+		},
+	)
 	if err != nil {
 		return "", "", nil
 	}
 
 	// update interface method
-	text, err = pathx.LoadTemplate(
-		category, updateMethodTemplateFile, template.UpdateMethod,
-	)
+	text, err = pathx.LoadTemplate(category, updateMethodTemplateFile, template.UpdateMethod)
 	if err != nil {
 		return "", "", err
 	}
 
-	updateMethodOutput, err := util.With("updateMethod").
-		Parse(text).
-		Execute(
-			map[string]interface{}{
-				"upperStartCamelObject": camelTableName,
-				"data":                  table,
-			},
-		)
+	updateMethodOutput, err := util.With("updateMethod").Parse(text).Execute(
+		map[string]interface{}{
+			"upperStartCamelObject": camelTableName,
+			"data":                  table,
+		},
+	)
 	if err != nil {
 		return "", "", nil
 	}
