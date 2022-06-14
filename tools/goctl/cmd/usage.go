1f083562d12886b0eac1ccff4937b64ff76e0e4f
anqiansong <anqiansong@gmail.com>
2022-06-14 09:51:34 +0800

    Color sub-commands for unix-like OS

--- tools/goctl/cmd/usage.go
+++ tools/goctl/cmd/usage.go
@@ -2,19 +2,33 @@ package cmd
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/fatih/color"
+	"github.com/zeromicro/go-zero/tools/goctl/vars"
 )
 
 func blue(s string) string {
+	if runtime.GOOS == vars.OsWindows {
+		return s
+	}
+
 	return color.New(color.FgHiBlue).Sprintf("%s", s)
 }
 
 func green(s string) string {
+	if runtime.GOOS == vars.OsWindows {
+		return s
+	}
+
 	return color.New(color.FgHiGreen).Sprintf("%s", s)
 }
 
 func rainbow(s string) string {
+	if runtime.GOOS == vars.OsWindows {
+		return s
+	}
+
 	s0 := s[0]
 	return color.New(color.Attribute(91+s0%6)).Sprintf("%s", s)
 }
