3b9ddf73c4e5e54b63c57c6468924ae8db56d0cc
anqiansong <anqiansong@gmail.com>
2022-06-19 23:29:28 +0800

    Remove redundant dependency

--- tools/goctl/cmd/usage.go
+++ tools/goctl/cmd/usage.go
@@ -4,16 +4,37 @@ import (
 	"fmt"
 	"runtime"
 
-	"github.com/fatih/color"
+	"github.com/logrusorgru/aurora"
 	"github.com/zeromicro/go-zero/tools/goctl/vars"
 )
+
+var colorRender = []func(v interface{}) string{
+	func(v interface{}) string {
+		return aurora.BrightRed(v).String()
+	},
+	func(v interface{}) string {
+		return aurora.BrightGreen(v).String()
+	},
+	func(v interface{}) string {
+		return aurora.BrightYellow(v).String()
+	},
+	func(v interface{}) string {
+		return aurora.BrightBlue(v).String()
+	},
+	func(v interface{}) string {
+		return aurora.BrightMagenta(v).String()
+	},
+	func(v interface{}) string {
+		return aurora.BrightCyan(v).String()
+	},
+}
 
 func blue(s string) string {
 	if runtime.GOOS == vars.OsWindows {
 		return s
 	}
 
-	return color.New(color.FgHiBlue).Sprintf("%s", s)
+	return aurora.BrightBlue(s).String()
 }
 
 func green(s string) string {
@@ -21,16 +42,15 @@ func green(s string) string {
 		return s
 	}
 
-	return color.New(color.FgHiGreen).Sprintf("%s", s)
+	return aurora.BrightGreen(s).String()
 }
 
 func rainbow(s string) string {
 	if runtime.GOOS == vars.OsWindows {
 		return s
 	}
-
 	s0 := s[0]
-	return color.New(color.Attribute(91+s0%6)).Sprintf("%s", s)
+	return colorRender[int(s0)%(len(colorRender)-1)](s)
 }
 
 // rpadx adds padding to the right of a string.
