26f80ac5a1324d9b1875b48fa67e381968af1a47
chenquan <chenquan.dev@gmail.com>
2022-06-22 20:56:23 +0800

    feat: support sse ignore timeout

--- rest/handler/timeouthandler.go
+++ rest/handler/timeouthandler.go
@@ -22,6 +22,8 @@ const (
 	reason                    = "Request Timeout"
 	headerUpgrade             = "Upgrade"
 	valueWebsocket            = "websocket"
+	headerAccept              = "Accept"
+	valueSSE                  = "text/event-stream"
 )
 
 // TimeoutHandler returns the handler with given timeout.
@@ -54,7 +56,9 @@ func (h *timeoutHandler) errorBody() string {
 }
 
 func (h *timeoutHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.Header.Get(headerUpgrade) == valueWebsocket {
+	if r.Header.Get(headerUpgrade) == valueWebsocket ||
+		// Server-Sent Event ignore timeout.
+		r.Header.Get(headerAccept) == valueSSE {
 		h.handler.ServeHTTP(w, r)
 		return
 	}
@@ -93,7 +97,7 @@ func (h *timeoutHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			tw.code = http.StatusOK
 		}
 		w.WriteHeader(tw.code)
-		w.Write(tw.wbuf.Bytes())
+		_, _ = w.Write(tw.wbuf.Bytes())
 	case <-ctx.Done():
 		tw.mu.Lock()
 		defer tw.mu.Unlock()
@@ -105,7 +109,7 @@ func (h *timeoutHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			} else {
 				w.WriteHeader(http.StatusServiceUnavailable)
 			}
-			io.WriteString(w, h.errorBody())
+			_, _ = io.WriteString(w, h.errorBody())
 		})
 		tw.timedOut = true
 	}
