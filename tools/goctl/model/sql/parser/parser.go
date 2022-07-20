1b51d0ce8298b9397ae620eae63cfc0fd005160f
anqiansong <anqiansong@gmail.com>
2022-07-20 22:49:41 +0800

    fix: fix #2102, #2108 (#2131)
    * g4 code generation
    
    * Update grammar
    
    * g4 code generation
    
    * fix #2108
    
    * fix #2102
    
    * Remove comments

--- tools/goctl/model/sql/parser/parser.go
+++ tools/goctl/model/sql/parser/parser.go
@@ -219,7 +219,7 @@ func convertColumns(columns []*parser.Column, primaryColumn string) (Primary, ma
 			}
 		}
 
-		dataType, err := converter.ConvertDataType(column.DataType.Type(), isDefaultNull)
+		dataType, err := converter.ConvertDataType(column.DataType.Type(), isDefaultNull, column.DataType.Unsigned())
 		if err != nil {
 			return Primary{}, nil, err
 		}
@@ -266,7 +266,8 @@ func (t *Table) ContainsTime() bool {
 // ConvertDataType converts mysql data type into golang data type
 func ConvertDataType(table *model.Table) (*Table, error) {
 	isPrimaryDefaultNull := table.PrimaryKey.ColumnDefault == nil && table.PrimaryKey.IsNullAble == "YES"
-	primaryDataType, err := converter.ConvertStringDataType(table.PrimaryKey.DataType, isPrimaryDefaultNull)
+	isPrimaryUnsigned := strings.Contains(table.PrimaryKey.DbColumn.ColumnType, "unsigned")
+	primaryDataType, err := converter.ConvertStringDataType(table.PrimaryKey.DataType, isPrimaryDefaultNull, isPrimaryUnsigned)
 	if err != nil {
 		return nil, err
 	}
@@ -345,7 +346,8 @@ func getTableFields(table *model.Table) (map[string]*Field, error) {
 	fieldM := make(map[string]*Field)
 	for _, each := range table.Columns {
 		isDefaultNull := each.ColumnDefault == nil && each.IsNullAble == "YES"
-		dt, err := converter.ConvertStringDataType(each.DataType, isDefaultNull)
+		isPrimaryUnsigned := strings.Contains(each.ColumnType, "unsigned")
+		dt, err := converter.ConvertStringDataType(each.DataType, isDefaultNull, isPrimaryUnsigned)
 		if err != nil {
 			return nil, err
 		}
