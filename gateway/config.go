e45fb4d7e04fedac5afe3bb276a4759eb2bfb758
mashouyue's m1max <mashouyue@toowow.cn>
2022-07-25 14:11:20 +0800

    support mulitple protoset files

--- gateway/config.go
+++ gateway/config.go
@@ -29,8 +29,9 @@ type (
 	Upstream struct {
 		// Grpc is the target of the upstream.
 		Grpc zrpc.RpcClientConf
-		// ProtoSet is the file of proto set, like hello.pb
-		ProtoSet string `json:",optional"`
+		// ProtoSets is the file list of proto set, like [hello.pb]
+		// if your proto file import another proto file, you need to write multi-file slice, like [hello.pb, common.pb]
+		ProtoSets []string `json:",optional"`
 		// Mapping is the mapping between gateway routes and Upstream rpc methods.
 		// Keep it blank if annotations are added in rpc methods.
 		Mapping []RouteMapping `json:",optional"`
