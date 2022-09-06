4d82ca2ac7bbb2f3840a1c23fd3fd25a1569a1ac
MarkJoyMa <markjoyma@163.com>
2022-09-06 18:16:38 +0800

    add comment

--- core/logx/writer.go
+++ core/logx/writer.go
@@ -256,8 +256,6 @@ func buildFields(fields ...LogField) []string {
 }
 
 func output(writer io.Writer, level string, val interface{}, fields ...LogField) {
-	//fields = append(fields, Field(callerKey, getCaller(callerDepth)))
-
 	switch atomic.LoadUint32(&encoding) {
 	case plainEncodingType:
 		writePlainAny(writer, level, val, buildFields(fields...)...)
