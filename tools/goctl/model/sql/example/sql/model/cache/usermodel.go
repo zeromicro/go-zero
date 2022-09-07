9248005e7ab1ff6e2e1dc7a8a378ef33e654f867
anqiansong <anqiansong@gmail.com>
2022-09-07 17:19:32 +0800

    Unrap nested structure for doc code generation

---
+++ tools/goctl/model/sql/example/sql/model/cache/usermodel.go
@@ -0,0 +1,27 @@
+package cache
+
+import (
+	"github.com/zeromicro/go-zero/core/stores/cache"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
+
+var _ UserModel = (*customUserModel)(nil)
+
+type (
+	// UserModel is an interface to be customized, add more methods here,
+	// and implement the added methods in customUserModel.
+	UserModel interface {
+		userModel
+	}
+
+	customUserModel struct {
+		*defaultUserModel
+	}
+)
+
+// NewUserModel returns a model for the database table.
+func NewUserModel(conn sqlx.SqlConn, c cache.CacheConf) UserModel {
+	return &customUserModel{
+		defaultUserModel: newUserModel(conn, c),
+	}
+}
