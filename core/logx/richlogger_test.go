5291d88b9f9a1547a2433a819ff11c33c2bb470b
kevin <wanjunfeng@gmail.com>
2022-09-17 00:14:10 +0800

    chore: simplify test code

--- core/logx/richlogger_test.go
+++ core/logx/richlogger_test.go
@@ -3,6 +3,7 @@ package logx
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 	"strings"
 	"sync/atomic"
@@ -209,6 +210,55 @@ func TestLogWithFields(t *testing.T) {
 	var val mockValue
 	assert.Nil(t, json.Unmarshal([]byte(w.String()), &val))
 	assert.Equal(t, "bar", val.Foo)
+}
+
+func TestLogWithCallerSkip(t *testing.T) {
+	w := new(mockWriter)
+	old := writer.Swap(w)
+	writer.lock.RLock()
+	defer func() {
+		writer.lock.RUnlock()
+		writer.Store(old)
+	}()
+
+	l := WithCallerSkip(1).WithCallerSkip(0)
+	p := func(v string) {
+		l.Info(v)
+	}
+
+	file, line := getFileLine()
+	p(testlog)
+	assert.True(t, w.Contains(fmt.Sprintf("%s:%d", file, line+1)))
+
+	w.Reset()
+	l = WithCallerSkip(0).WithCallerSkip(1)
+	file, line = getFileLine()
+	p(testlog)
+	assert.True(t, w.Contains(fmt.Sprintf("%s:%d", file, line+1)))
+}
+
+func TestWithFields(t *testing.T) {
+	w := new(mockWriter)
+	old := writer.Swap(w)
+	writer.lock.RLock()
+	defer func() {
+		writer.lock.RUnlock()
+		writer.Store(old)
+	}()
+
+	l := WithFields(Field("a", "b")).WithFields(Field("c", "d"))
+	p := func(v string) {
+		l.Info(v)
+	}
+
+	p(testlog)
+	var v struct {
+		A string `json:"a"`
+		C string `json:"c"`
+	}
+	assert.Nil(t, json.Unmarshal([]byte(w.String()), &v))
+	assert.Equal(t, "b", v.A, w.String())
+	assert.Equal(t, "d", v.C, w.String())
 }
 
 func validate(t *testing.T, body string, expectedTrace, expectedSpan bool) {
