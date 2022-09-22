8d904ee338ec36981e01a85c08a3d102c7e60bd4
Benson Yan <bensonfx@outlook.com>
2022-09-22 23:02:39 +0800

    fix header and path type ts gen

--- core/logx/writer.go
+++ core/logx/writer.go
@@ -292,8 +292,6 @@ func wrapLevelWithColor(level string) string {
 		colour = color.FgBlue
 	case levelSlow:
 		colour = color.FgYellow
-	case levelDebug:
-		colour = color.FgYellow
 	case levelStat:
 		colour = color.FgGreen
 	}
