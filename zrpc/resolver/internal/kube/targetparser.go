7110b0382c6ec85d0ba36df340bd5360561f01e2
bigrocs <bigrocs@qq.com>
2022-11-12 19:41:41 +0800

    k8s 端口自动发现

--- zrpc/resolver/internal/kube/targetparser.go
+++ zrpc/resolver/internal/kube/targetparser.go
@@ -1,49 +1,52 @@
 package kube
 
 import (
-	"fmt"
-	"strconv"
-	"strings"
+ "fmt"
+ "strconv"
+ "strings"
 
-	"github.com/zeromicro/go-zero/zrpc/resolver/internal/targets"
-	"google.golang.org/grpc/resolver"
+ "github.com/zeromicro/go-zero/zrpc/resolver/internal/targets"
+ "google.golang.org/grpc/resolver"
 )
 
 const (
-	colon            = ":"
-	defaultNamespace = "default"
+ colon            = ":"
+ defaultNamespace = "default"
 )
 
 var emptyService Service
 
 // Service represents a service with namespace, name and port.
 type Service struct {
-	Namespace string
-	Name      string
-	Port      int
+ Namespace string
+ Name      string
+ Port      int
 }
 
 // ParseTarget parses the resolver.Target.
 func ParseTarget(target resolver.Target) (Service, error) {
-	var service Service
-	service.Namespace = targets.GetAuthority(target)
-	if len(service.Namespace) == 0 {
-		service.Namespace = defaultNamespace
-	}
+ var service Service
+ service.Namespace = targets.GetAuthority(target)
+ if len(service.Namespace) == 0 {
+  service.Namespace = defaultNamespace
+ }
 
-	endpoints := targets.GetEndpoints(target)
-	segs := strings.SplitN(endpoints, colon, 2)
-	if len(segs) < 2 {
-		return emptyService, fmt.Errorf("bad endpoint: %s", endpoints)
-	}
+ endpoints := targets.GetEndpoints(target)
+ if strings.Contains(endpoints, colon) {
+  segs := strings.SplitN(endpoints, colon, 2)
+  if len(segs) < 2 {
+   return emptyService, fmt.Errorf("bad endpoint: %s", endpoints)
+  }
 
-	service.Name = segs[0]
-	port, err := strconv.Atoi(segs[1])
-	if err != nil {
-		return emptyService, err
-	}
+  service.Name = segs[0]
+  port, err := strconv.Atoi(segs[1])
+  if err != nil {
+   return emptyService, err
+  }
 
-	service.Port = port
-
-	return service, nil
+  service.Port = port
+ } else {
+  service.Name = endpoints
+ }
+ return service, nil
 }
