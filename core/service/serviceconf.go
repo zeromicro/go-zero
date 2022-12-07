e908bfd8bab4ee0af8bc85142c570df6ecc4bcdf
dylan.wang <dylan.wang@yijinin.com>
2022-12-07 17:59:11 +0800

    feat: mv devServer to internal

--- core/service/serviceconf.go
+++ core/service/serviceconf.go
@@ -3,13 +3,13 @@ package service
 import (
 	"log"
 
-	"github.com/zeromicro/go-zero/core/devserver"
 	"github.com/zeromicro/go-zero/core/load"
 	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/core/proc"
 	"github.com/zeromicro/go-zero/core/prometheus"
 	"github.com/zeromicro/go-zero/core/stat"
 	"github.com/zeromicro/go-zero/core/trace"
+	"github.com/zeromicro/go-zero/internal/devserver"
 )
 
 const (
