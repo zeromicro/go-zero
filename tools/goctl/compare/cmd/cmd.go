e0ca3e8cb7ab770c1fc9f6add2399c7bcde5fa51
chenjieping <chenjieping@kezaihui.com>
2023-01-06 11:11:14 +0800

    fix: replace goctl ExactValidArgs to MatchAll

--- tools/goctl/compare/cmd/cmd.go
+++ tools/goctl/compare/cmd/cmd.go
@@ -9,7 +9,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "compare",
 	Short: "Compare the goctl commands generated results between urfave and cobra",
-	Args:  cobra.ExactValidArgs(1),
+	Args:  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
 	Run: func(cmd *cobra.Command, args []string) {
 		dir := args[0]
 		testdata.MustRun(dir)
