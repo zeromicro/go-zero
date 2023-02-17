e20fe9acacd3fa5bfc940798b577d23519a0e8d5
xiandong <xiandong@italki.com>
2023-02-17 12:58:31 +0800

    add kindJaegerUdp

--- core/trace/config.go
+++ core/trace/config.go
@@ -1,27 +1,32 @@
 package trace
 
-import "fmt"
+import (
+	"strings"
+)
 
 // TraceName represents the tracing name.
 const TraceName = "go-zero"
 
 // A Config is an opentelemetry config.
 type Config struct {
-	Name      string  `json:",optional"`
-	AgentHost string  `json:",optional"`
-	AgentPort string  `json:",optional"`
-	Endpoint  string  `json:",optional"`
-	Sampler   float64 `json:",default=1.0"`
-	Batcher   string  `json:",default=jaeger,options=jaeger|jaegerudp|zipkin|otlpgrpc|otlphttp"`
+	Name     string  `json:",optional"`
+	Endpoint string  `json:",optional"`
+	Sampler  float64 `json:",default=1.0"`
+	Batcher  string  `json:",default=jaeger,options=jaeger|jaegerudp|zipkin|otlpgrpc|otlphttp"`
 }
 
-func (c *Config) isAgentEndPoint() bool {
-	return len(c.AgentHost) != 0 && len(c.AgentPort) != 0
-}
-
-func (c *Config) getEndpoint() string {
-	if c.isAgentEndPoint() {
-		return fmt.Sprintf("%s:%s", c.AgentHost, c.AgentPort)
+func (c *Config) getEndpointHost() string {
+	EndpointSlice := strings.Split(c.Endpoint, ":")
+	if len(EndpointSlice) > 0 {
+		return strings.TrimSpace(EndpointSlice[0])
 	}
-	return c.Endpoint
+	return ""
+}
+
+func (c *Config) getEndpointPort() string {
+	EndpointSlice := strings.Split(c.Endpoint, ":")
+	if len(EndpointSlice) > 1 {
+		return strings.TrimSpace(EndpointSlice[1])
+	}
+	return ""
 }
