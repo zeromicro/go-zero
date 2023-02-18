0a0927444228e63d5f29626aebb3ec4a882c3377
chenquan <chenquan.dev@gmail.com>
2023-02-18 15:22:54 +0800

    feat: support read-write separation under the leader-follower database

---
+++ core/stores/sqlx/multiple.go
@@ -0,0 +1,230 @@
+package sqlx
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+var _ MultipleSqlConn = (*multipleSqlConn)(nil)
+
+type (
+	DBConf struct {
+		Leader    string
+		Followers []string
+		// BackSource back to source when all slave data is not available.
+		BackSource bool
+	}
+
+	MultipleSqlConn interface {
+		SqlConn
+		// LeaderDB returns a leader db.
+		LeaderDB() (conn SqlConn, err error)
+		// FollowerDB returns a follower db.
+		FollowerDB() (conn SqlConn, err error)
+	}
+
+	multipleSqlConn struct {
+		leader         SqlConn
+		enableFollower bool
+		p2cPicker      picker
+		conf           DBConf
+	}
+)
+
+// MustNewMultipleSqlConn returns a MultipleSqlConn.
+func MustNewMultipleSqlConn(driverName string, conf DBConf) MultipleSqlConn {
+	conn, err := NewMultipleSqlConn(driverName, conf)
+	logx.Must(err)
+	return conn
+}
+
+// NewMultipleSqlConn returns a MultipleSqlConn.
+func NewMultipleSqlConn(driverName string, conf DBConf) (MultipleSqlConn, error) {
+	if err := conf.verify(); err != nil {
+		return nil, err
+	}
+
+	leader := NewSqlConn(driverName, conf.Leader)
+	if err := pingDB(leader); err != nil {
+		return nil, fmt.Errorf("%s:%w", conf.Leader, err)
+	}
+
+	followers := make([]SqlConn, 0, len(conf.Followers))
+	for _, datasource := range conf.Followers {
+		follower := NewSqlConn(driverName, datasource)
+		if err := pingDB(follower); err != nil {
+			return nil, fmt.Errorf("%s:%w", conf.Leader, err)
+		}
+
+		followers = append(followers, follower)
+	}
+
+	return &multipleSqlConn{
+		leader:         leader,
+		enableFollower: len(followers) != 0,
+		p2cPicker:      newP2cPicker(followers),
+	}, nil
+}
+
+func (m *multipleSqlConn) Exec(query string, args ...any) (sql.Result, error) {
+	return m.ExecCtx(context.Background(), query, args...)
+}
+
+func (m *multipleSqlConn) ExecCtx(ctx context.Context, query string, args ...any) (sql.Result, error) {
+	return m.leader.ExecCtx(ctx, query, args...)
+}
+
+func (m *multipleSqlConn) Prepare(query string) (StmtSession, error) {
+	return m.PrepareCtx(context.Background(), query)
+}
+
+func (m *multipleSqlConn) PrepareCtx(ctx context.Context, query string) (StmtSession, error) {
+	return m.leader.PrepareCtx(ctx, query)
+}
+
+func (m *multipleSqlConn) QueryRow(v any, query string, args ...any) error {
+	return m.QueryRowCtx(context.Background(), v, query, args...)
+}
+
+func (m *multipleSqlConn) QueryRowCtx(ctx context.Context, v any, query string, args ...any) error {
+	db := m.getQueryDb(query)
+	return db.query(func(conn SqlConn) error {
+		return conn.QueryRowCtx(ctx, v, query, args...)
+	})
+}
+
+func (m *multipleSqlConn) QueryRowPartial(v any, query string, args ...any) error {
+	return m.QueryRowPartialCtx(context.Background(), v, query, args...)
+}
+
+func (m *multipleSqlConn) QueryRowPartialCtx(ctx context.Context, v any, query string, args ...any) error {
+	db := m.getQueryDb(query)
+	return db.query(func(conn SqlConn) error {
+		return conn.QueryRowPartialCtx(ctx, v, query, args...)
+	})
+}
+
+func (m *multipleSqlConn) QueryRows(v any, query string, args ...any) error {
+	return m.QueryRowsCtx(context.Background(), v, query, args...)
+}
+
+func (m *multipleSqlConn) QueryRowsCtx(ctx context.Context, v any, query string, args ...any) error {
+	db := m.getQueryDb(query)
+	return db.query(func(conn SqlConn) error {
+		return conn.QueryRowsCtx(ctx, v, query, args...)
+	})
+}
+
+func (m *multipleSqlConn) QueryRowsPartial(v any, query string, args ...any) error {
+	return m.QueryRowsPartialCtx(context.Background(), v, query, args...)
+}
+
+func (m *multipleSqlConn) QueryRowsPartialCtx(ctx context.Context, v any, query string, args ...any) error {
+	db := m.getQueryDb(query)
+	return db.query(func(conn SqlConn) error {
+		return conn.QueryRowsPartialCtx(ctx, v, query, args...)
+	})
+}
+
+func (m *multipleSqlConn) RawDB() (*sql.DB, error) {
+	return m.leader.RawDB()
+}
+
+func (m *multipleSqlConn) Transact(fn func(Session) error) error {
+	return m.TransactCtx(context.Background(), func(_ context.Context, session Session) error {
+		return fn(session)
+	})
+}
+
+func (m *multipleSqlConn) TransactCtx(ctx context.Context, fn func(context.Context, Session) error) error {
+	return m.leader.TransactCtx(ctx, fn)
+}
+
+func (m *multipleSqlConn) containSelect(query string) bool {
+	query = strings.TrimSpace(query)
+	if len(query) >= 6 {
+		return strings.EqualFold(query[:6], "select")
+	}
+
+	return false
+}
+
+func (m *multipleSqlConn) getQueryDb(query string) queryDb {
+	if m.containSelect(query) && m.enableFollower {
+		result, err := m.p2cPicker.pick()
+		if err == nil {
+			return queryDb{
+				conn: result.conn,
+				done: result.done,
+			}
+		}
+
+		if !m.conf.BackSource {
+			return queryDb{
+				error: err,
+			}
+		}
+	}
+
+	return queryDb{conn: m.leader}
+}
+
+func (m *multipleSqlConn) LeaderDB() (conn SqlConn, err error) {
+	return m.leader, nil
+}
+
+func (m *multipleSqlConn) FollowerDB() (SqlConn, error) {
+	result, err := m.p2cPicker.pick()
+	if err != nil {
+		return nil, err
+	}
+
+	return result.conn, nil
+}
+
+// -------------
+
+type queryDb struct {
+	conn  SqlConn
+	error error
+	done  func(err error)
+}
+
+func (q *queryDb) query(query func(conn SqlConn) error) (err error) {
+	if q.error != nil {
+		return q.error
+	}
+	defer func() {
+		if q.done != nil {
+			q.done(err)
+		}
+	}()
+
+	return query(q.conn)
+}
+
+// -------------
+
+func (c DBConf) verify() error {
+	if c.Leader == "" {
+		return errors.New("leader cannot be empty")
+	}
+
+	return nil
+}
+
+// -------------
+
+func pingDB(conn SqlConn) error {
+	db, err := conn.RawDB()
+	if err != nil {
+		return err
+	}
+
+	return db.Ping()
+}
---
+++ core/stores/sqlx/picker.go
@@ -0,0 +1,209 @@
+package sqlx
+
+import (
+	"errors"
+	"fmt"
+	"math"
+	"math/rand"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/core/syncx"
+	"github.com/zeromicro/go-zero/core/timex"
+)
+
+const (
+	decayTime       = int64(time.Second * 10) // default value from finagle
+	forcePick       = int64(time.Second)
+	initSuccess     = 1000
+	throttleSuccess = initSuccess / 2
+	penalty         = int64(math.MaxInt32)
+	pickTimes       = 3
+	logInterval     = time.Minute
+)
+
+var ErrNoFollowerAvailable = errors.New("no follower available")
+
+type (
+	picker interface {
+		pick() (*pickResult, error)
+	}
+	pickResult struct {
+		conn SqlConn
+		done func(err error)
+	}
+	doneInfo struct {
+		err error
+	}
+	p2cPicker struct {
+		conns []*subConn
+		r     *rand.Rand
+		stamp *syncx.AtomicDuration
+		lock  sync.Mutex
+	}
+)
+
+func newP2cPicker(followers []SqlConn) *p2cPicker {
+	var conns []*subConn
+	for i, follower := range followers {
+		conns = append(conns, &subConn{
+			success: initSuccess,
+			db:      i,
+			conn:    follower,
+		})
+	}
+
+	return &p2cPicker{
+		conns: conns,
+		r:     rand.New(rand.NewSource(time.Now().UnixNano())),
+		stamp: syncx.NewAtomicDuration(),
+	}
+}
+
+func (p *p2cPicker) pick() (*pickResult, error) {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	var chosen *subConn
+	switch len(p.conns) {
+	case 0:
+		return nil, ErrNoFollowerAvailable
+	case 1:
+		chosen = p.choose(p.conns[0], nil)
+	case 2:
+		chosen = p.choose(p.conns[0], p.conns[1])
+	default:
+		var node1, node2 *subConn
+		for i := 0; i < pickTimes; i++ {
+			a := p.r.Intn(len(p.conns))
+			b := p.r.Intn(len(p.conns) - 1)
+			if b >= a {
+				b++
+			}
+			node1 = p.conns[a]
+			node2 = p.conns[b]
+			if node1.healthy() && node2.healthy() {
+				break
+			}
+		}
+
+		chosen = p.choose(node1, node2)
+	}
+
+	atomic.AddInt64(&chosen.inflight, 1)
+	atomic.AddInt64(&chosen.requests, 1)
+
+	return &pickResult{
+		conn: chosen.conn,
+		done: p.buildDoneFunc(chosen),
+	}, nil
+}
+
+func (p *p2cPicker) buildDoneFunc(c *subConn) func(err error) {
+	start := int64(timex.Now())
+	return func(err error) {
+		atomic.AddInt64(&c.inflight, -1)
+		now := timex.Now()
+		last := atomic.SwapInt64(&c.last, int64(now))
+		td := int64(now) - last
+		if td < 0 {
+			td = 0
+		}
+		w := math.Exp(float64(-td) / float64(decayTime))
+		lag := int64(now) - start
+		if lag < 0 {
+			lag = 0
+		}
+		olag := atomic.LoadUint64(&c.lag)
+		if olag == 0 {
+			w = 0
+		}
+		atomic.StoreUint64(&c.lag, uint64(float64(olag)*w+float64(lag)*(1-w)))
+		success := initSuccess
+
+		if err != nil && !acceptable(err) {
+			success = 0
+		}
+
+		osucc := atomic.LoadUint64(&c.success)
+		atomic.StoreUint64(&c.success, uint64(float64(osucc)*w+float64(success)*(1-w)))
+
+		stamp := p.stamp.Load()
+		if now-stamp >= logInterval {
+			if p.stamp.CompareAndSwap(stamp, now) {
+				p.logStats()
+			}
+		}
+	}
+}
+
+func (p *p2cPicker) choose(c1, c2 *subConn) *subConn {
+	start := int64(timex.Now())
+	if c2 == nil {
+		atomic.StoreInt64(&c1.pick, start)
+		return c1
+	}
+
+	if c1.load() > c2.load() {
+		c1, c2 = c2, c1
+	}
+
+	pick := atomic.LoadInt64(&c2.pick)
+	if start-pick > forcePick && atomic.CompareAndSwapInt64(&c2.pick, pick, start) {
+		return c2
+	}
+
+	atomic.StoreInt64(&c1.pick, start)
+	return c1
+}
+
+func (p *p2cPicker) logStats() {
+	var stats []string
+
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	for _, conn := range p.conns {
+		stats = append(stats, fmt.Sprintf("db: %d, load: %d, reqs: %d",
+			conn.db, conn.load(), atomic.SwapInt64(&conn.requests, 0)))
+	}
+
+	logx.Statf("follower-db - p2c - %s", strings.Join(stats, "; "))
+}
+
+type subConn struct {
+	lag      uint64
+	inflight int64
+	success  uint64
+	requests int64
+	last     int64
+	pick     int64
+	db       int
+	conn     SqlConn
+}
+
+func (c *subConn) healthy() bool {
+	return atomic.LoadUint64(&c.success) > throttleSuccess
+}
+
+func (c *subConn) load() int64 {
+	// plus one to avoid multiply zero
+	lag := int64(math.Sqrt(float64(atomic.LoadUint64(&c.lag) + 1)))
+	load := lag * (atomic.LoadInt64(&c.inflight) + 1)
+	if load == 0 {
+		return penalty
+	}
+
+	return load
+}
+
+func acceptable(err error) bool {
+	if errors.Is(err, ErrNotFound) {
+		return true
+	}
+
+	return false
+}
