56fa48e9da135f21c37e9c530c0a30c1b57fc17d
kevin <wanjunfeng@gmail.com>
2023-02-20 23:21:20 +0800

    chore: reformat code

--- tools/goctl/api/tsgen/gen.go
+++ tools/goctl/api/tsgen/gen.go
@@ -7,7 +7,6 @@ import (
 	"github.com/logrusorgru/aurora"
 	"github.com/spf13/cobra"
 	"github.com/zeromicro/go-zero/core/logx"
-
 	"github.com/zeromicro/go-zero/tools/goctl/api/parser"
 	"github.com/zeromicro/go-zero/tools/goctl/util/pathx"
 )
