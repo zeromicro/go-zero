96998ae570224074c7a097fbe57ff6c615d53c50
yangjinheng <420123641@qq.com>
2023-02-27 14:06:10 +0800

    Update timeouthandler.go

--- rest/handler/timeouthandler.go
+++ rest/handler/timeouthandler.go
@@ -125,6 +125,14 @@ type timeoutWriter struct {
 
 var _ http.Pusher = (*timeoutWriter)(nil)
 
+func (tw *timeoutWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	if hijacked, ok := tw.w.(http.Hijacker); ok {
+		return hijacked.Hijack()
+	}
+
+	return nil, nil, errors.New("server doesn't support hijacking")
+}
+
 // Header returns the underline temporary http.Header.
 func (tw *timeoutWriter) Header() http.Header { return tw.h }
 
