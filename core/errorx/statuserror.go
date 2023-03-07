1f4905a036d7c5120579ab5a0d2545a1a200f962
Ryan SU <1137661202@qq.com>
2023-03-07 09:27:12 +0800

    feat: add status error to errorx and remove error msg

---
+++ core/errorx/statuserror.go
@@ -0,0 +1,31 @@
+package errorx
+
+import (
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// NewInternalError returns status error with Internal error code.
+func NewInternalError(msg string) error {
+	return status.Error(codes.Internal, msg)
+}
+
+// NewInvalidArgumentError returns status error with InvalidArgument error code.
+func NewInvalidArgumentError(msg string) error {
+	return status.Error(codes.InvalidArgument, msg)
+}
+
+// NewNotFoundError returns status error with NotFound error code.
+func NewNotFoundError(msg string) error {
+	return status.Error(codes.NotFound, msg)
+}
+
+// NewAlreadyExistsError returns status error with AlreadyExists error code.
+func NewAlreadyExistsError(msg string) error {
+	return status.Error(codes.AlreadyExists, msg)
+}
+
+// NewUnauthenticatedError returns status error with Unauthenticated error code.
+func NewUnauthenticatedError(msg string) error {
+	return status.Error(codes.Unauthenticated, msg)
+}
