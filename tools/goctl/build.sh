0a8a96c7f4c52004bb76216fce997c2ab2a63e2d
Ryan SU <yuansu.china.work@gmail.com>
2023-03-28 08:47:27 +0800

    fix: optimize makefile

--- tools/goctl/build.sh
+++
@@ -1,7 +0,0 @@
-git pull
-
-go mod tidy
-
-go build -ldflags="-s -w" -o goctls goctl.go
-
-mv goctls "$GOPATH"/bin/goctls
--- tools/goctl/build_win.sh
+++
@@ -1,9 +0,0 @@
-# use git bash
-
-git pull
-
-go mod tidy
-
-GOOS=windows go build -ldflags="-s -w" -o goctls.exe goctl.go
-
-mv goctls.exe $GOPATH/bin/goctls.exe
