20e2ff3f66e7d3813ce3b96c7264965b860da7d1
NorthLan <6995syu@163.com>
2023-03-29 10:32:04 +0800

    style: go fmt.

--- tools/goctl/util/protox/proto.go
+++ tools/goctl/util/protox/proto.go
@@ -15,64 +15,64 @@
 package protox
 
 import (
-    "fmt"
-    "strings"
+	"fmt"
+	"strings"
 
-    "github.com/emicklei/proto"
+	"github.com/emicklei/proto"
 )
 
 func FindBeginEndOfService(service, serviceName string) (begin, mid, end int) {
-    beginIndex := strings.Index(service, serviceName)
-    begin, end = -1, -1
-    if beginIndex > 0 {
-        for i := beginIndex; i < len(service); i++ {
-            if service[i] == '}' {
-                end = i
-                break
-            } else if service[i] == '{' {
-                mid = i
-            }
-        }
+	beginIndex := strings.Index(service, serviceName)
+	begin, end = -1, -1
+	if beginIndex > 0 {
+		for i := beginIndex; i < len(service); i++ {
+			if service[i] == '}' {
+				end = i
+				break
+			} else if service[i] == '{' {
+				mid = i
+			}
+		}
 
-        for i := beginIndex; i >= 0; i-- {
-            if service[i] == 's' {
-                begin = i
-                break
-            }
-        }
-    }
-    return begin, mid, end
+		for i := beginIndex; i >= 0; i-- {
+			if service[i] == 's' {
+				begin = i
+				break
+			}
+		}
+	}
+	return begin, mid, end
 }
 
 var ProtoField *ProtoFieldData
 
 type ProtoFieldData struct {
-    Name     string
-    Type     string
-    Repeated bool
-    Optional bool
+	Name     string
+	Type     string
+	Repeated bool
+	Optional bool
 }
 
 type MessageVisitor struct {
-    proto.NoopVisitor
+	proto.NoopVisitor
 }
 
 func (m MessageVisitor) VisitNormalField(i *proto.NormalField) {
-    ProtoField.Name = i.Field.Name
-    ProtoField.Type = i.Field.Type
-    ProtoField.Repeated = i.Repeated
-    ProtoField.Optional = i.Optional
+	ProtoField.Name = i.Field.Name
+	ProtoField.Type = i.Field.Type
+	ProtoField.Repeated = i.Repeated
+	ProtoField.Optional = i.Optional
 }
 
 func GenCommentString(comments []string, space bool) string {
-    var commentsString strings.Builder
-    var spaceString string
-    if space {
-        spaceString = "  "
-    }
+	var commentsString strings.Builder
+	var spaceString string
+	if space {
+		spaceString = "  "
+	}
 
-    for _, v := range comments {
-        commentsString.WriteString(fmt.Sprintf("%s// %s\n", spaceString, v))
-    }
-    return commentsString.String()
+	for _, v := range comments {
+		commentsString.WriteString(fmt.Sprintf("%s// %s\n", spaceString, v))
+	}
+	return commentsString.String()
 }
