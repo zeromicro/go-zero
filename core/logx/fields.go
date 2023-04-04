9732da591a8af4d801c7d45c41ab0c707493a50e
reatang <tangtang1251@qq.com>
2023-04-04 14:48:24 +0800

    fix. log context not unique

--- core/logx/fields.go
+++ core/logx/fields.go
@@ -12,7 +12,7 @@ var (
 	globalFieldsLock sync.Mutex
 )
 
-type contextKey struct{}
+type contextKey int
 
 // AddGlobalFields adds global fields.
 func AddGlobalFields(fields ...LogField) {
@@ -29,16 +29,16 @@ func AddGlobalFields(fields ...LogField) {
 
 // ContextWithFields returns a new context with the given fields.
 func ContextWithFields(ctx context.Context, fields ...LogField) context.Context {
-	if val := ctx.Value(fieldsContextKey); val != nil {
+	if val := ctx.Value(&fieldsContextKey); val != nil {
 		if arr, ok := val.([]LogField); ok {
 			allFields := make([]LogField, 0, len(arr)+len(fields))
 			allFields = append(allFields, arr...)
 			allFields = append(allFields, fields...)
-			return context.WithValue(ctx, fieldsContextKey, allFields)
+			return context.WithValue(ctx, &fieldsContextKey, allFields)
 		}
 	}
 
-	return context.WithValue(ctx, fieldsContextKey, fields)
+	return context.WithValue(ctx, &fieldsContextKey, fields)
 }
 
 // WithFields returns a new logger with the given fields.
