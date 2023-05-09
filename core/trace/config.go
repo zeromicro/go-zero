b1c4c4736f6abe53387bcb24b769b0f6effc5082
Kevin Wan <wanjunfeng@gmail.com>
2023-05-09 22:58:40 +0800

    chore: better comments (#3232)

--- core/trace/config.go
+++ core/trace/config.go
@@ -5,9 +5,12 @@ const TraceName = "go-zero"
 
 // A Config is an opentelemetry config.
 type Config struct {
-	Name        string            `json:",optional"`
-	Endpoint    string            `json:",optional"`
-	Sampler     float64           `json:",default=1.0"`
-	Batcher     string            `json:",default=jaeger,options=jaeger|zipkin|otlpgrpc|otlphttp"`
-	OtlpHeaders map[string]string `json:",optional"` // uptrace-dsn: 'http://project2_secret_token@localhost:14317/2'
+	Name     string  `json:",optional"`
+	Endpoint string  `json:",optional"`
+	Sampler  float64 `json:",default=1.0"`
+	Batcher  string  `json:",default=jaeger,options=jaeger|zipkin|otlpgrpc|otlphttp"`
+	// OtlpHeaders represents the headers for OTLP gRPC or HTTP transport.
+	// For example:
+	//  uptrace-dsn: 'http://project2_secret_token@localhost:14317/2'
+	OtlpHeaders map[string]string `json:",optional"`
 }
