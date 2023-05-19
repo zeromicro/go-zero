a9aac7e4200ab39885200fcec48b33add425a0b8
Kevin Wan <wanjunfeng@gmail.com>
2023-05-19 23:29:30 +0800

    chore: add more tests (#3265)

--- core/stat/internal/cgroup_linux_test.go
+++ core/stat/internal/cgroup_linux_test.go
@@ -10,3 +10,18 @@ func TestRunningInUserNS(t *testing.T) {
 	// should be false in docker
 	assert.False(t, runningInUserNS())
 }
+
+func TestCgroupV1(t *testing.T) {
+	if isCgroup2UnifiedMode() {
+		cg, err := currentCgroupV1()
+		assert.NoError(t, err)
+		_, err = cg.cpus()
+		assert.Error(t, err)
+		_, err = cg.cpuPeriodUs()
+		assert.Error(t, err)
+		_, err = cg.cpuQuotaUs()
+		assert.Error(t, err)
+		_, err = cg.usageAllCpus()
+		assert.Error(t, err)
+	}
+}
