1222bd8d0df25ae437e23b7b0d6976ca0c870cb7
kevin <wanjunfeng@gmail.com>
2023-05-19 10:36:16 +0800

    chore: fix data race

--- core/prof/runtime_test.go
+++ core/prof/runtime_test.go
@@ -1,18 +1,16 @@
 package prof
 
 import (
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/zeromicro/go-zero/internal/iotest"
 )
 
 func TestDisplayStats(t *testing.T) {
-	iotest.RunTest(t, func() {
-		DisplayStats(time.Millisecond * 10)
-		time.Sleep(time.Millisecond * 50)
-	}, func(s string) {
-		assert.Contains(t, s, "Goroutines: ")
-	})
+	var buf strings.Builder
+	displayStatsWithWriter(&buf, time.Millisecond*10)
+	time.Sleep(time.Millisecond * 50)
+	assert.Contains(t, buf.String(), "Goroutines: ")
 }
