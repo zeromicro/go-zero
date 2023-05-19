889e182c57ae706bdae531abaa7bc61aff2b8323
kevin <wanjunfeng@gmail.com>
2023-05-19 10:54:52 +0800

    chore: fix data race

--- core/prof/runtime_test.go
+++ core/prof/runtime_test.go
@@ -2,6 +2,7 @@ package prof
 
 import (
 	"strings"
+	"sync"
 	"testing"
 	"time"
 
@@ -9,8 +10,27 @@ import (
 )
 
 func TestDisplayStats(t *testing.T) {
-	var buf strings.Builder
-	displayStatsWithWriter(&buf, time.Millisecond*10)
+	writer := &threadSafeBuffer{
+		buf: strings.Builder{},
+	}
+	displayStatsWithWriter(writer, time.Millisecond*10)
 	time.Sleep(time.Millisecond * 50)
-	assert.Contains(t, buf.String(), "Goroutines: ")
+	assert.Contains(t, writer.String(), "Goroutines: ")
+}
+
+type threadSafeBuffer struct {
+	buf  strings.Builder
+	lock sync.Mutex
+}
+
+func (b *threadSafeBuffer) String() string {
+	b.lock.Lock()
+	defer b.lock.Unlock()
+	return b.buf.String()
+}
+
+func (b *threadSafeBuffer) Write(p []byte) (n int, err error) {
+	b.lock.Lock()
+	defer b.lock.Unlock()
+	return b.buf.Write(p)
 }
