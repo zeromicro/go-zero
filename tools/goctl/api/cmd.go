a7f1291c43c06ed63c65b3fd8bf665bde48d2056
Ryan Su <yuansu.china.work@gmail.com>
2023-06-16 08:58:23 +0800

    fix: remove unnecessary lang support
    Signed-off-by: Ryan Su <yuansu.china.work@gmail.com>

--- tools/goctl/api/cmd.go
+++ tools/goctl/api/cmd.go
@@ -4,14 +4,10 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/zeromicro/go-zero/tools/goctl/api/apigen"
-	"github.com/zeromicro/go-zero/tools/goctl/api/dartgen"
 	"github.com/zeromicro/go-zero/tools/goctl/api/docgen"
 	"github.com/zeromicro/go-zero/tools/goctl/api/format"
 	"github.com/zeromicro/go-zero/tools/goctl/api/gogen"
-	"github.com/zeromicro/go-zero/tools/goctl/api/javagen"
-	"github.com/zeromicro/go-zero/tools/goctl/api/ktgen"
 	"github.com/zeromicro/go-zero/tools/goctl/api/new"
-	"github.com/zeromicro/go-zero/tools/goctl/api/tsgen"
 	"github.com/zeromicro/go-zero/tools/goctl/api/validate"
 	"github.com/zeromicro/go-zero/tools/goctl/config"
 	"github.com/zeromicro/go-zero/tools/goctl/internal/cobrax"
@@ -21,17 +17,13 @@ import (
 var (
 	// Cmd describes an api command.
 	Cmd       = cobrax.NewCommand("api", cobrax.WithRunE(apigen.CreateApiTemplate))
-	dartCmd   = cobrax.NewCommand("dart", cobrax.WithRunE(dartgen.DartCommand))
 	docCmd    = cobrax.NewCommand("doc", cobrax.WithRunE(docgen.DocCommand))
 	formatCmd = cobrax.NewCommand("format", cobrax.WithRunE(format.GoFormatApi))
 	goCmd     = cobrax.NewCommand("go", cobrax.WithRunE(gogen.GoCommand))
 	newCmd    = cobrax.NewCommand("new", cobrax.WithRunE(new.CreateServiceCommand),
 		cobrax.WithArgs(cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs)))
 	validateCmd = cobrax.NewCommand("validate", cobrax.WithRunE(validate.GoValidateApi))
-	javaCmd     = cobrax.NewCommand("java", cobrax.WithRunE(javagen.JavaCommand), cobrax.WithHidden())
-	ktCmd       = cobrax.NewCommand("kt", cobrax.WithRunE(ktgen.KtCommand))
 	pluginCmd   = cobrax.NewCommand("plugin", cobrax.WithRunE(plugin.PluginCommand))
-	tsCmd       = cobrax.NewCommand("ts", cobrax.WithRunE(tsgen.TsCommand))
 
 	protoCmd = cobrax.NewCommand("proto", cobrax.WithRunE(gogen.GenCRUDLogicByProto))
 
@@ -41,15 +33,11 @@ var (
 func init() {
 	var (
 		apiCmdFlags      = Cmd.Flags()
-		dartCmdFlags     = dartCmd.Flags()
 		docCmdFlags      = docCmd.Flags()
 		formatCmdFlags   = formatCmd.Flags()
 		goCmdFlags       = goCmd.Flags()
-		javaCmdFlags     = javaCmd.Flags()
-		ktCmdFlags       = ktCmd.Flags()
 		newCmdFlags      = newCmd.Flags()
 		pluginCmdFlags   = pluginCmd.Flags()
-		tsCmdFlags       = tsCmd.Flags()
 		validateCmdFlags = validateCmd.Flags()
 		protoCmdFlags    = protoCmd.Flags()
 		entCmdFlags      = entCmd.Flags()
@@ -59,12 +47,6 @@ func init() {
 	apiCmdFlags.StringVar(&apigen.VarStringHome, "home")
 	apiCmdFlags.StringVar(&apigen.VarStringRemote, "remote")
 	apiCmdFlags.StringVar(&apigen.VarStringBranch, "branch")
-
-	dartCmdFlags.StringVar(&dartgen.VarStringDir, "dir")
-	dartCmdFlags.StringVar(&dartgen.VarStringAPI, "api")
-	dartCmdFlags.BoolVar(&dartgen.VarStringLegacy, "legacy")
-	dartCmdFlags.StringVar(&dartgen.VarStringHostname, "hostname")
-	dartCmdFlags.StringVar(&dartgen.VarStringScheme, "scheme")
 
 	docCmdFlags.StringVar(&docgen.VarStringDir, "dir")
 	docCmdFlags.StringVar(&docgen.VarStringOutput, "o")
@@ -84,13 +66,6 @@ func init() {
 	goCmdFlags.BoolVarP(&gogen.VarBoolUseCasbin, "casbin", "c")
 	goCmdFlags.BoolVarP(&gogen.VarBoolUseI18n, "i18n", "i")
 
-	javaCmdFlags.StringVar(&javagen.VarStringDir, "dir")
-	javaCmdFlags.StringVar(&javagen.VarStringAPI, "api")
-
-	ktCmdFlags.StringVar(&ktgen.VarStringDir, "dir")
-	ktCmdFlags.StringVar(&ktgen.VarStringAPI, "api")
-	ktCmdFlags.StringVar(&ktgen.VarStringPKG, "pkg")
-
 	newCmdFlags.StringVar(&new.VarStringHome, "home")
 	newCmdFlags.StringVar(&new.VarStringRemote, "remote")
 	newCmdFlags.StringVar(&new.VarStringBranch, "branch")
@@ -109,11 +84,6 @@ func init() {
 	pluginCmdFlags.StringVar(&plugin.VarStringDir, "dir")
 	pluginCmdFlags.StringVar(&plugin.VarStringAPI, "api")
 	pluginCmdFlags.StringVar(&plugin.VarStringStyle, "style")
-
-	tsCmdFlags.StringVar(&tsgen.VarStringDir, "dir")
-	tsCmdFlags.StringVar(&tsgen.VarStringAPI, "api")
-	tsCmdFlags.StringVar(&tsgen.VarStringCaller, "caller")
-	tsCmdFlags.BoolVar(&tsgen.VarBoolUnWrap, "unwrap")
 
 	validateCmdFlags.StringVar(&validate.VarStringAPI, "api")
 
@@ -145,15 +115,11 @@ func init() {
 	entCmdFlags.StringVarPWithDefaultValue(&gogen.VarStringJSONStyle, "json_style", "j", "goZero")
 
 	// Add sub-commands
-	Cmd.AddCommand(dartCmd)
 	Cmd.AddCommand(docCmd)
 	Cmd.AddCommand(formatCmd)
 	Cmd.AddCommand(goCmd)
-	Cmd.AddCommand(javaCmd)
-	Cmd.AddCommand(ktCmd)
 	Cmd.AddCommand(newCmd)
 	Cmd.AddCommand(pluginCmd)
-	Cmd.AddCommand(tsCmd)
 	Cmd.AddCommand(validateCmd)
 	Cmd.AddCommand(protoCmd)
 	Cmd.AddCommand(entCmd)
