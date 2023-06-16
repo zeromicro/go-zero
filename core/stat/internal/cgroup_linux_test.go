2ea704a2ad85fcde9bf3ff45b38fef1bb6c44a42
kevin <wanjunfeng@gmail.com>
2023-06-16 23:47:09 +0800

    chore: add more tests

--- core/stat/internal/cgroup_linux_test.go
+++ core/stat/internal/cgroup_linux_test.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -23,5 +24,27 @@ func TestCgroupV1(t *testing.T) {
 		assert.Error(t, err)
 		_, err = cg.usageAllCpus()
 		assert.Error(t, err)
+	}
+}
+
+func TestParseUints(t *testing.T) {
+	tests := []struct {
+		input string
+		want  []uint64
+		err   error
+	}{
+		{"", nil, nil},
+		{"1,2,3", []uint64{1, 2, 3}, nil},
+		{"1-3", []uint64{1, 2, 3}, nil},
+		{"1-3,5,7-9", []uint64{1, 2, 3, 5, 7, 8, 9}, nil},
+		{"foo", nil, fmt.Errorf("strconv.ParseUint: parsing \"foo\": invalid syntax")},
+		{"1-bar", nil, fmt.Errorf("strconv.ParseUint: parsing \"1-bar\": invalid syntax")},
+		{"3-1", nil, fmt.Errorf("cgroup: bad int list format: 3-1")},
+	}
+
+	for _, tt := range tests {
+		got, err := parseUints(tt.input)
+		assert.Equal(t, tt.err, err)
+		assert.Equal(t, tt.want, got)
 	}
 }
