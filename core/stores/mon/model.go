34c0bf1b529037b1bb829d7067a23ffc1e991d2a
liqi <419822933@qq.com>
2023-09-25 14:55:53 +0800

    add customer breaker for mongo

--- core/stores/mon/model.go
+++ core/stores/mon/model.go
@@ -52,6 +52,14 @@ func NewModel(uri, db, collection string, opts ...Option) (*Model, error) {
 
 	name := strings.Join([]string{uri, collection}, "/")
 	brk := breaker.GetBreaker(uri)
+
+	if withOutBreaker != nil {
+		breaker.NoBreakerFor(uri)
+	}
+	if withBreaker != nil {
+		brk = withBreaker.(breaker.Breaker)
+	}
+	
 	coll := newCollection(cli.Database(db).Collection(collection), brk)
 	return newModel(name, cli, coll, brk, opts...), nil
 }
