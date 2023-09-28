4caf83928d7f02009b9b302688886ea95918cef4
Ryan Su <yuansu.china.work@gmail.com>
2023-09-28 17:19:23 +0800

    chore: upgrade jaeger

--- core/trace/agent_test.go
+++ core/trace/agent_test.go
@@ -23,11 +23,6 @@ func TestStartAgent(t *testing.T) {
 	c1 := Config{
 		Name: "foo",
 	}
-	c2 := Config{
-		Name:     "bar",
-		Endpoint: endpoint1,
-		Batcher:  kindJaeger,
-	}
 	c3 := Config{
 		Name:     "any",
 		Endpoint: endpoint2,
@@ -55,16 +50,6 @@ func TestStartAgent(t *testing.T) {
 		},
 		OtlpHttpPath: "/v1/traces",
 	}
-	c7 := Config{
-		Name:     "UDP",
-		Endpoint: endpoint5,
-		Batcher:  kindJaeger,
-	}
-	c8 := Config{
-		Disabled: true,
-		Endpoint: endpoint6,
-		Batcher:  kindJaeger,
-	}
 	c9 := Config{
 		Name:     "file",
 		Endpoint: endpoint71,
@@ -78,13 +63,10 @@ func TestStartAgent(t *testing.T) {
 
 	StartAgent(c1)
 	StartAgent(c1)
-	StartAgent(c2)
 	StartAgent(c3)
 	StartAgent(c4)
 	StartAgent(c5)
 	StartAgent(c6)
-	StartAgent(c7)
-	StartAgent(c8)
 	StartAgent(c9)
 	StartAgent(c10)
 	defer StopAgent()
