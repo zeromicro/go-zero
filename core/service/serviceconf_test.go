8f2cb13f0d99e7383ac42aecd7f4787cf1f9b16d
anqiansong <anqiansong@gmail.com>
2023-10-17 20:06:59 +0800

    sort imports

--- core/service/serviceconf_test.go
+++ core/service/serviceconf_test.go
@@ -1,11 +1,11 @@
 package service
 
 import (
-	"github.com/zeromicro/go-zero/internal/devserver"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/internal/devserver"
 )
 
 func TestServiceConf(t *testing.T) {
