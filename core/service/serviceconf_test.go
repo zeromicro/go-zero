423597a01c17bfc6670f04f17f77eb0278a3b007
kesonan <anqiansong@gmail.com>
2023-10-17 23:38:21 +0800

    feat: export devserver.Config (#3638)

--- core/service/serviceconf_test.go
+++ core/service/serviceconf_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/internal/devserver"
 )
 
 func TestServiceConf(t *testing.T) {
@@ -14,6 +15,10 @@ func TestServiceConf(t *testing.T) {
 			Mode: "console",
 		},
 		Mode: "dev",
+		DevServer: devserver.Config{
+			Port:       6470,
+			HealthPath: "/healthz",
+		},
 	}
 	c.MustSetUp()
 }
