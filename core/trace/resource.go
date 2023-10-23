920b055b51e93ac869a01d2154154bcf877f94cc
Kevin Wan <wanjunfeng@gmail.com>
2023-10-23 21:18:10 +0800

    Update resource.go

--- core/trace/resource.go
+++ core/trace/resource.go
@@ -4,6 +4,7 @@ import "go.opentelemetry.io/otel/attribute"
 
 var attrResources = make([]attribute.KeyValue, 0)
 
+// AddResources add more resources in addition to configured trace name.
 func AddResources(attrs ...attribute.KeyValue) {
 	attrResources = append(attrResources, attrs...)
 }
