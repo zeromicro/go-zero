5216537b461a2d0d1471bce7e77b3e7ecaf7775c
POABOB <zxc752166@yahoo.com.tw>
2023-12-11 02:12:22 +0800

    fix: prevent a crash if there is a unique key constraint with a nil field.

--- tools/goctl/model/sql/parser/parser.go
+++ tools/goctl/model/sql/parser/parser.go
@@ -149,6 +149,10 @@ func Parse(filename, database string, strict bool) ([]*Table, error) {
 
 		for indexName, each := range uniqueKeyMap {
 			for _, columnName := range each {
+				// Prevent a crash if there is a unique key constraint with a nil field.
+				if fieldM[columnName] == nil {
+					return nil, fmt.Errorf("table %s: unique key with error column name[%s]", e.Name, columnName)
+				}
 				uniqueIndex[indexName] = append(uniqueIndex[indexName], fieldM[columnName])
 			}
 		}
