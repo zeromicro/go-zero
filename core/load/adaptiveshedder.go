329caab43835c8d40483c9901d8516392f83f7fd
kevin <wanjunfeng@gmail.com>
2024-02-12 16:58:54 +0800

    optimize: add cpu related factor to suppress requests

--- core/load/adaptiveshedder.go
+++ core/load/adaptiveshedder.go
@@ -23,6 +23,7 @@ const (
 	// moving average hyperparameter beta for calculating requests on the fly
 	flyingBeta            = 0.9
 	coolOffDuration       = time.Second
+	cpuMax                = 1000 // millicpu
 	millisecondsPerSecond = 1000
 )
 
@@ -150,16 +151,16 @@ func (as *adaptiveShedder) highThru() bool {
 	as.avgFlyingLock.Lock()
 	avgFlying := as.avgFlying
 	as.avgFlyingLock.Unlock()
-	maxFlight := as.maxFlight()
-	return int64(avgFlying) > maxFlight && atomic.LoadInt64(&as.flying) > maxFlight
+	maxFlight := as.maxFlight() * as.overloadFactor()
+	return avgFlying > maxFlight && float64(atomic.LoadInt64(&as.flying)) > maxFlight
 }
 
-func (as *adaptiveShedder) maxFlight() int64 {
+func (as *adaptiveShedder) maxFlight() float64 {
 	// windows = buckets per second
 	// maxQPS = maxPASS * windows
 	// minRT = min average response time in milliseconds
-	// maxQPS * minRT / milliseconds_per_second
-	return int64(math.Max(1, float64(as.maxPass())*as.minRt()*as.windowScale))
+	// allowedFlying = maxQPS * minRT / milliseconds_per_second
+	return math.Max(1, float64(as.maxPass())*as.minRt()*as.windowScale)
 }
 
 func (as *adaptiveShedder) maxPass() int64 {
@@ -175,6 +176,8 @@ func (as *adaptiveShedder) maxPass() int64 {
 }
 
 func (as *adaptiveShedder) minRt() float64 {
+	// if no requests in previous windows, return defaultMinRt,
+	// its a reasonable large value to avoid dropping requests.
 	result := defaultMinRt
 
 	as.rtCounter.Reduce(func(b *collection.Bucket) {
@@ -189,6 +192,11 @@ func (as *adaptiveShedder) minRt() float64 {
 	})
 
 	return result
+}
+
+func (as *adaptiveShedder) overloadFactor() float64 {
+	factor := (cpuMax - float64(stat.CpuUsage())) / (cpuMax - float64(as.cpuThreshold))
+	return math.Max(1, factor)
 }
 
 func (as *adaptiveShedder) shouldDrop() bool {
