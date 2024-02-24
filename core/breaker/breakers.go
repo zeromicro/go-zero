a59324a6b86a24a7bc9ac8344ab36282b0b9ad13
kevin <wanjunfeng@gmail.com>
2024-02-25 00:44:35 +0800

    feat: support breaker with sql statements

--- core/breaker/breakers.go
+++ core/breaker/breakers.go
@@ -59,7 +59,7 @@ func GetBreaker(name string) Breaker {
 // NoBreakerFor disables the circuit breaker for the given name.
 func NoBreakerFor(name string) {
 	lock.Lock()
-	breakers[name] = newNopBreaker()
+	breakers[name] = NopBreaker()
 	lock.Unlock()
 }
 
