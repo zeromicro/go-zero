25821bdee67975bf7926aaba07bd67ee74c10842
Kevin Wan <wanjunfeng@gmail.com>
2024-03-03 00:17:38 +0800

    chore: coding style (#3960)

--- core/stores/mon/options.go
+++ core/stores/mon/options.go
@@ -19,17 +19,17 @@ var (
 )
 
 type (
-	options = mopt.ClientOptions
-
 	// Option defines the method to customize a mongo model.
 	Option func(opts *options)
 
-	// RegisterType A struct store With custom type and Encoder/Decoder
-	RegisterType struct {
+	// TypeCodec is a struct that stores specific type Encoder/Decoder.
+	TypeCodec struct {
 		ValueType reflect.Type
 		Encoder   bsoncodec.ValueEncoder
 		Decoder   bsoncodec.ValueDecoder
 	}
+
+	options = mopt.ClientOptions
 )
 
 // DisableLog disables logging of mongo commands, includes info and slow logs.
@@ -48,12 +48,6 @@ func SetSlowThreshold(threshold time.Duration) {
 	slowThreshold.Set(threshold)
 }
 
-func defaultTimeoutOption() Option {
-	return func(opts *options) {
-		opts.SetTimeout(defaultTimeout)
-	}
-}
-
 // WithTimeout set the mon client operation timeout.
 func WithTimeout(timeout time.Duration) Option {
 	return func(opts *options) {
@@ -61,14 +55,20 @@ func WithTimeout(timeout time.Duration) Option {
 	}
 }
 
-// WithRegistry set the Registry to convert custom type to mongo primitive type more easily.
-func WithRegistry(registerType ...RegisterType) Option {
+// WithTypeCodec registers TypeCodecs to convert custom types.
+func WithTypeCodec(typeCodecs ...TypeCodec) Option {
 	return func(opts *options) {
 		registry := bson.NewRegistry()
-		for _, v := range registerType {
+		for _, v := range typeCodecs {
 			registry.RegisterTypeEncoder(v.ValueType, v.Encoder)
 			registry.RegisterTypeDecoder(v.ValueType, v.Decoder)
 		}
 		opts.SetRegistry(registry)
+	}
+}
+
+func defaultTimeoutOption() Option {
+	return func(opts *options) {
+		opts.SetTimeout(defaultTimeout)
 	}
 }
