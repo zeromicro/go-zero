3cc8d5eaf11bf5ebb7aeb2bdc17ecd694a559778
kevin <wanjunfeng@gmail.com>
2024-03-30 12:34:27 +0800

    chore: fix test failure

--- rest/httpx/util.go
+++ rest/httpx/util.go
@@ -2,6 +2,7 @@ package httpx
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 )
 
@@ -21,10 +22,16 @@ func GetFormValues(r *http.Request) (map[string]any, error) {
 
 	params := make(map[string]any, len(r.Form))
 	for name := range r.Form {
-		if len(r.Form[name]) > 1 {
+		fmt.Println(name, len(r.Form[name]))
+		switch len(r.Form[name]) {
+		case 1:
+			formValue := r.Form.Get(name)
+			if len(formValue) > 0 {
+				params[name] = formValue
+			}
+		default:
+			// len(r.Form[name]) > 1, never be 0
 			params[name] = r.Form[name]
-		} else if len(r.Form[name]) == 1 {
-			params[name] = r.Form.Get(name)
 		}
 	}
 
