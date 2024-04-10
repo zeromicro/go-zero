8290051e438f50d78ad2dded2c9c010bed171823
anqiansong <anqiansong@gmail.com>
2024-04-10 15:27:31 +0800

    Add model config

--- tools/goctl/model/sql/converter/types.go
+++ tools/goctl/model/sql/converter/types.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	"github.com/zeromicro/ddl-parser/parser"
+	"github.com/zeromicro/go-zero/tools/goctl/config"
 )
 
 var unsignedTypeMap = map[string]string{
@@ -16,61 +17,60 @@ var unsignedTypeMap = map[string]string{
 }
 
 var commonMysqlDataTypeMapInt = map[int]string{
-	// For consistency, all integer types are converted to int64
 	// number
-	parser.Bit:       "byte",
-	parser.TinyInt:   "int64",
-	parser.SmallInt:  "int64",
-	parser.MediumInt: "int64",
-	parser.Int:       "int64",
-	parser.MiddleInt: "int64",
-	parser.Int1:      "int64",
-	parser.Int2:      "int64",
-	parser.Int3:      "int64",
-	parser.Int4:      "int64",
-	parser.Int8:      "int64",
-	parser.Integer:   "int64",
-	parser.BigInt:    "int64",
-	parser.Float:     "float64",
-	parser.Float4:    "float64",
-	parser.Float8:    "float64",
-	parser.Double:    "float64",
-	parser.Decimal:   "float64",
-	parser.Dec:       "float64",
-	parser.Fixed:     "float64",
-	parser.Numeric:   "float64",
-	parser.Real:      "float64",
+	parser.Bit:       "bit",
+	parser.TinyInt:   "tinyint",
+	parser.SmallInt:  "smallint",
+	parser.MediumInt: "mediumint",
+	parser.Int:       "int",
+	parser.MiddleInt: "middleint",
+	parser.Int1:      "int1",
+	parser.Int2:      "int2",
+	parser.Int3:      "int3",
+	parser.Int4:      "int4",
+	parser.Int8:      "int8",
+	parser.Integer:   "integer",
+	parser.BigInt:    "bigint",
+	parser.Float:     "float",
+	parser.Float4:    "float4",
+	parser.Float8:    "float8",
+	parser.Double:    "double",
+	parser.Decimal:   "decimal",
+	parser.Dec:       "dec",
+	parser.Fixed:     "fixed",
+	parser.Numeric:   "numeric",
+	parser.Real:      "real",
 	// date&time
-	parser.Date:      "time.Time",
-	parser.DateTime:  "time.Time",
-	parser.Timestamp: "time.Time",
-	parser.Time:      "string",
-	parser.Year:      "int64",
+	parser.Date:      "date",
+	parser.DateTime:  "datetime",
+	parser.Timestamp: "timestamp",
+	parser.Time:      "time",
+	parser.Year:      "year",
 	// string
-	parser.Char:            "string",
-	parser.VarChar:         "string",
-	parser.NVarChar:        "string",
-	parser.NChar:           "string",
-	parser.Character:       "string",
-	parser.LongVarChar:     "string",
-	parser.LineString:      "string",
-	parser.MultiLineString: "string",
-	parser.Binary:          "string",
-	parser.VarBinary:       "string",
-	parser.TinyText:        "string",
-	parser.Text:            "string",
-	parser.MediumText:      "string",
-	parser.LongText:        "string",
-	parser.Enum:            "string",
-	parser.Set:             "string",
-	parser.Json:            "string",
-	parser.Blob:            "string",
-	parser.LongBlob:        "string",
-	parser.MediumBlob:      "string",
-	parser.TinyBlob:        "string",
+	parser.Char:            "char",
+	parser.VarChar:         "varchar",
+	parser.NVarChar:        "nvarchar",
+	parser.NChar:           "nchar",
+	parser.Character:       "character",
+	parser.LongVarChar:     "longvarchar",
+	parser.LineString:      "linestring",
+	parser.MultiLineString: "multilinestring",
+	parser.Binary:          "binary",
+	parser.VarBinary:       "varbinary",
+	parser.TinyText:        "tinytext",
+	parser.Text:            "text",
+	parser.MediumText:      "mediumtext",
+	parser.LongText:        "longtext",
+	parser.Enum:            "enum",
+	parser.Set:             "set",
+	parser.Json:            "json",
+	parser.Blob:            "blob",
+	parser.LongBlob:        "longblob",
+	parser.MediumBlob:      "mediumblob",
+	parser.TinyBlob:        "tinyblob",
 	// bool
 	parser.Bool:    "bool",
-	parser.Boolean: "bool",
+	parser.Boolean: "boolean",
 }
 
 var commonMysqlDataTypeMapString = map[string]string{
@@ -144,28 +144,56 @@ var commonMysqlDataTypeMapString = map[string]string{
 }
 
 // ConvertDataType converts mysql column type into golang type
-func ConvertDataType(dataBaseType int, isDefaultNull, unsigned, strict bool) (string, error) {
+func ConvertDataType(dataBaseType int, isDefaultNull, unsigned, strict bool) (string, string, error) {
 	tp, ok := commonMysqlDataTypeMapInt[dataBaseType]
 	if !ok {
-		return "", fmt.Errorf("unsupported database type: %v", dataBaseType)
+		return "", "", fmt.Errorf("unsupported database type: %v", dataBaseType)
 	}
 
-	return mayConvertNullType(tp, isDefaultNull, unsigned, strict), nil
+	goType, thirdPkg, _, err := ConvertStringDataType(tp, isDefaultNull, unsigned, strict)
+	return goType, thirdPkg, err
 }
 
 // ConvertStringDataType converts mysql column type into golang type
 func ConvertStringDataType(dataBaseType string, isDefaultNull, unsigned, strict bool) (
-	goType string, isPQArray bool, err error) {
+	goType string, thirdPkg string, isPQArray bool, err error) {
+
+	customTp, thirdImport := convertDatatypeWithConfig(dataBaseType, isDefaultNull, unsigned)
+	if len(customTp) != 0 {
+		return customTp, thirdImport, false, nil
+	}
+
 	tp, ok := commonMysqlDataTypeMapString[strings.ToLower(dataBaseType)]
 	if !ok {
-		return "", false, fmt.Errorf("unsupported database type: %s", dataBaseType)
+		return "", "", false, fmt.Errorf("unsupported database type: %s", dataBaseType)
 	}
 
 	if strings.HasPrefix(dataBaseType, "_") {
-		return tp, true, nil
+		return tp, "", true, nil
 	}
 
-	return mayConvertNullType(tp, isDefaultNull, unsigned, strict), false, nil
+	return mayConvertNullType(tp, isDefaultNull, unsigned, strict), "", false, nil
+}
+
+func convertDatatypeWithConfig(dataBaseType string, isDefaultNull, unsigned bool) (string, string) {
+	if config.ExternalConfig == nil {
+		return "", ""
+	}
+	opt, ok := config.ExternalConfig.Model.TypesMap[strings.ToLower(dataBaseType)]
+	if !ok || (len(opt.Type) == 0 && len(opt.UnsignedType) == 0 && len(opt.NullType) == 0) {
+		return "", ""
+	}
+
+	if isDefaultNull {
+		if len(opt.NullType) != 0 {
+			return opt.NullType, opt.Pkg
+		}
+	} else if unsigned {
+		if len(opt.UnsignedType) != 0 {
+			return opt.UnsignedType, opt.Pkg
+		}
+	}
+	return opt.Type, opt.Pkg
 }
 
 func mayConvertNullType(goDataType string, isDefaultNull, unsigned, strict bool) string {
