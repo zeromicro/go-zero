b57842444b0a4de52df88e6f30bfd6ab6ffd19e7
kevin <wanjunfeng@gmail.com>
2024-04-28 21:59:05 +0800

    feat: add fx.ParallelErr

--- core/fx/parallel.go
+++ core/fx/parallel.go
@@ -1,6 +1,9 @@
 package fx
 
-import "github.com/zeromicro/go-zero/core/threading"
+import (
+	"github.com/zeromicro/go-zero/core/errorx"
+	"github.com/zeromicro/go-zero/core/threading"
+)
 
 // Parallel runs fns parallelly and waits for done.
 func Parallel(fns ...func()) {
@@ -9,4 +12,21 @@ func Parallel(fns ...func()) {
 		group.RunSafe(fn)
 	}
 	group.Wait()
+}
+
+func ParallelErr(fns ...func() error) error {
+	var be errorx.BatchError
+
+	group := threading.NewRoutineGroup()
+	for _, fn := range fns {
+		f := fn
+		group.RunSafe(func() {
+			if err := f(); err != nil {
+				be.Add(err)
+			}
+		})
+	}
+	group.Wait()
+
+	return be.Err()
 }
