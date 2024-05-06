7a5b51df3cb69d34b8c41f31bbc13ddc17804a78
kevin <wanjunfeng@gmail.com>
2024-05-06 23:13:00 +0800

    fix: etcd scheme on grpc resolver, because of grpc resolver logic changed

--- zrpc/resolver/internal/discovbuilder.go
+++ zrpc/resolver/internal/discovbuilder.go
@@ -9,16 +9,44 @@ import (
 	"google.golang.org/grpc/resolver"
 )
 
-type discovBuilder struct{}
+type discovBuilder struct {
+	cc     resolver.ClientConn
+	update func()
+}
 
 func (b *discovBuilder) Build(target resolver.Target, cc resolver.ClientConn, _ resolver.BuildOptions) (
 	resolver.Resolver, error) {
+	b.cc = cc
+	if err := b.updateState(target); err != nil {
+		return nil, err
+	}
+
+	return &nopResolver{cc: cc}, nil
+}
+
+func (b *discovBuilder) Scheme() string {
+	return DiscovScheme
+}
+
+func (b *discovBuilder) updateState(target resolver.Target) error {
+	if b.update == nil {
+		if err := b.buildEndpointsUpdater(target); err != nil {
+			return err
+		}
+	}
+
+	b.update()
+
+	return nil
+}
+
+func (b *discovBuilder) buildEndpointsUpdater(target resolver.Target) error {
 	hosts := strings.FieldsFunc(targets.GetAuthority(target), func(r rune) bool {
 		return r == EndpointSepChar
 	})
 	sub, err := discov.NewSubscriber(hosts, targets.GetEndpoints(target))
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	update := func() {
@@ -29,18 +57,14 @@ func (b *discovBuilder) Build(target resolver.Target, cc resolver.ClientConn, _ 
 				Addr: val,
 			})
 		}
-		if err := cc.UpdateState(resolver.State{
+		if err := b.cc.UpdateState(resolver.State{
 			Addresses: addrs,
 		}); err != nil {
 			logx.Error(err)
 		}
 	}
 	sub.AddListener(update)
-	update()
+	b.update = update
 
-	return &nopResolver{cc: cc}, nil
-}
-
-func (b *discovBuilder) Scheme() string {
-	return DiscovScheme
+	return nil
 }
