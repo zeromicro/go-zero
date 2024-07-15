c14aa3bab5e6825013331dd7ba23a1df59cdfc28
anqiansong <anqiansong@gmail.com>
2024-07-15 11:41:08 +0800

    Add web

--- tools/goctl/tool/web/web/src/components/api/API.tsx
+++ tools/goctl/tool/web/web/src/components/api/API.tsx
@@ -1,32 +1,72 @@
 import React, { useState } from "react";
-import { Layout, Row, Col } from "antd";
+import { Layout, Row, Col, Flex } from "antd";
 import "../../Base.css";
 import "./API.css";
 import FormPanel from "./form/FormPanel";
 import CodePanel from "./form/CodePanel";
 
 const App: React.FC = () => {
-  const [code, setCode] = useState("");
+  const [code, setCode] = useState(`{
+  "code": 0,
+  "msg": "This is a json placeholder",
+  "data": {
+    "author": {
+      "first": "an",
+      "last": "keson"
+    },
+    "github": "https://github.com/kesonan/converter",
+    "star": 1800,
+    "nickname": "kesonan",
+    "description": "A Golang Software Developer engineer from ShangHai.",
+    "active": true,
+    "tags": ["goctl","go-zero","go","java","android"],
+    "projects": [
+      {
+        "organization": "zeromicro",
+        "name": "go-zero",
+        "github": "https://github.com/zeromicro/go-zero",
+        "stars": 28200,
+        "description": "A cloud-native Go microservices framework with cli tool for productivityA cloud-native Go microservices framework with cli tool for productivity."
+      },
+      {
+        "organization": "kesonan",
+        "name": "github-compare",
+        "github": "https://github.com/kesonan/github-compare",
+        "stars": 149,
+        "description": "A GitHub repositories statistics command-line tool for the terminal"
+      },
+      {
+        "organization": "kesonan",
+        "name": "goimportx",
+        "github": "https://github.com/kesonan/goimportx",
+        "stars": 10,
+        "description": "A tool to help you manage your go imports."
+      }
+    ],
+    "others": [],
+    "job": {}
+  }
+}`);
   return (
     <Layout className="api">
-      <Row wrap className="api-container" gutter={1}>
-        <Col span={12} className={"api-form-panel"}>
+      <Flex vertical={false} wrap className="api-container" gap={1}>
+        <div className={"api-form-panel"}>
           <FormPanel
             onBuild={(data) => {
               const js = JSON.stringify(data);
               setCode(js);
             }}
           />
-        </Col>
-        <Col span={12} className={"api-code-panel"}>
+        </div>
+        <div className={"api-code-panel"}>
           <CodePanel
             onChange={(code) => {
               setCode(code);
             }}
             value={code}
           />
-        </Col>
-      </Row>
+        </div>
+      </Flex>
     </Layout>
   );
 };
