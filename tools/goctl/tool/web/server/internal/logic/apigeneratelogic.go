f0224a5afd2e150622f630446e5827e16eba9c68
kesonan <anqiansong@gmail.com>
2024-07-16 12:59:49 +0800

    Add web

--- tools/goctl/tool/web/server/internal/logic/apigeneratelogic.go
+++ tools/goctl/tool/web/server/internal/logic/apigeneratelogic.go
@@ -2,11 +2,15 @@ package logic
 
 import (
 	"context"
+	"errors"
+	"github.com/zeromicro/go-zero/tools/goctl/util"
 
 	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/tools/goctl/tool/web/server/internal/svc"
 	"github.com/zeromicro/go-zero/tools/goctl/tool/web/server/internal/types"
 )
+
+var errMissingServiceName = errors.New("missing service name")
 
 type ApiGenerateLogic struct {
 	logx.Logger
@@ -23,7 +27,15 @@ func NewApiGenerateLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ApiGe
 }
 
 func (l *ApiGenerateLogic) ApiGenerate(req *types.APIGenerateRequest) (resp *types.APIGenerateResponse, err error) {
-
-
+	if err := l.validateAPIGenerateRequest(req); err != nil {
+		return nil, err
+	}
 	return
+}
+
+func (l *ApiGenerateLogic) validateAPIGenerateRequest(req *types.APIGenerateRequest) error {
+	if util.IsEmptyStringOrWhiteSpace(req.Name) {
+		return errMissingServiceName
+	}
+
 }
