64e3aeda55e8ce654d0507aa4af461700641ba15
kesonan <anqiansong@gmail.com>
2024-08-03 22:22:51 +0800

    Add goctl version to code header (#4293)

--- tools/goctl/model/mongo/generate/generate.go
+++ tools/goctl/model/mongo/generate/generate.go
@@ -5,6 +5,7 @@ import (
 	"path/filepath"
 
 	"github.com/zeromicro/go-zero/tools/goctl/config"
+	"github.com/zeromicro/go-zero/tools/goctl/internal/version"
 	"github.com/zeromicro/go-zero/tools/goctl/model/mongo/template"
 	"github.com/zeromicro/go-zero/tools/goctl/util"
 	"github.com/zeromicro/go-zero/tools/goctl/util/format"
@@ -59,6 +60,7 @@ func generateModel(ctx *Context) error {
 			"Type":      stringx.From(t).Title(),
 			"lowerType": stringx.From(t).Untitle(),
 			"Cache":     ctx.Cache,
+			"version":   version.BuildVersion,
 		}, output, true); err != nil {
 			return err
 		}
