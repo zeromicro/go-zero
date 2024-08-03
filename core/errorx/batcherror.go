b962776aa67f2b794c410e317e6433fe622b6f5e
kevin <wanjunfeng@gmail.com>
2024-08-03 13:47:16 +0800

    refactor: simplify BatchError

--- core/errorx/batcherror.go
+++ core/errorx/batcherror.go
@@ -5,17 +5,13 @@ import (
 	"sync"
 )
 
-type (
-	// A BatchError is an error that can hold multiple errors.
-	BatchError struct {
-		errs []error
-		lock sync.RWMutex
-	}
+// BatchError is an error that can hold multiple errors.
+type BatchError struct {
+	errs []error
+	lock sync.RWMutex
+}
 
-	errorArray []error
-)
-
-// Add adds errs to be, nil errors are ignored.
+// Add adds one or more non-nil errors to the BatchError instance.
 func (be *BatchError) Add(errs ...error) {
 	be.lock.Lock()
 	defer be.lock.Unlock()
@@ -27,15 +23,17 @@ func (be *BatchError) Add(errs ...error) {
 	}
 }
 
-// Err returns an error that represents all errors.
+// Err returns an error that represents all accumulated errors.
+// It returns nil if there are no errors.
 func (be *BatchError) Err() error {
 	be.lock.RLock()
 	defer be.lock.RUnlock()
 
+	// If there are no non-nil errors, errors.Join(...) returns nil.
 	return errors.Join(be.errs...)
 }
 
-// NotNil checks if any error inside.
+// NotNil checks if there is at least one error inside the BatchError.
 func (be *BatchError) NotNil() bool {
 	be.lock.RLock()
 	defer be.lock.RUnlock()
