1f26ae17a870c13f4f4b90e01b974245aac14aa6
yangjinheng <420123641@qq.com>
2024-10-16 18:20:56 +0800

    fix the source code directory after the soft link
    To solve the problem of the disk full of code, I used soft links to move the directory where the code is saved, but the import path of the code generated by goctl is wrong
    
    The problem occurs in tools/goctl/rpc/generator/mkdir.go SetPbDir()
    
    d.ctx.Dir calculates the absolute path
    pbDir does not calculate the absolute path
    strings.TrimPrefix tries to remove d.ctx.Dir from pbDir but does not get a match

--- tools/goctl/rpc/generator/genpb.go
+++ tools/goctl/rpc/generator/genpb.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 
 	"github.com/zeromicro/go-zero/tools/goctl/rpc/execx"
+	"github.com/zeromicro/go-zero/tools/goctl/util/pathx"
 )
 
 // GenPb generates the pb.go file, which is a layer of packaging for protoc to generate gprc,
