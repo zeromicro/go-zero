b6ee69c000affc5e5061c7102bf4e47d88d89bd6
chaosannals <chaosannals@outlook.com>
2024-12-10 15:33:17 +0800

    phpgen.

---
+++ tools/goctl/api/phpgen/ApiBaseClient.php
@@ -0,0 +1,144 @@
+<?php
+
+class ApiBaseClient
+{
+    private $host;
+    private $port;
+
+    public function __construct(
+        $host,
+        $port
+    ) {
+        $this->host = $host;
+        $this->port = $port;
+    }
+
+    public function getHost()
+    {
+        return $this->host;
+    }
+    public function getPort()
+    {
+        return $this->port;
+    }
+
+    public function getAddress()
+    {
+        return "$this->host:$this->port";
+    }
+
+    protected function request(
+        $path, // 请求路径
+        $method, // 请求方法 get post delete put
+        $params, // 请求路径参数
+        $query,  // 请求字符串
+        $headers, // 头部字段
+        $body // 内容体
+    ) {
+        $address = $this->getAddress();
+        if (!$headers) {
+            $headers = [];
+        } else {
+            $headers = $headers->toAssocArray();
+        }
+
+        // path
+        if ($params) {
+            $path = self::replacePathParams($path, $params->toAssocArray());
+        }
+        $url = "$address$path";
+
+        // query
+        if ($query) {
+            $queryString = $query->toQueryString();
+            $url = "$url?$queryString";
+        }
+
+        $ch = curl_init();
+
+        // 2. 设置请求选项, 包括具体的url
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1); // HTTP/1.1
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 4); /* 在发起连接前等待的时间，如果设置为0，则无限等待 */
+        curl_setopt($ch, CURLOPT_TIMEOUT, 20); /* 设置cURL允许执行的最长秒数 */
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+
+        // POST
+        if ($method == 'post') {
+            curl_setopt($ch, CURLOPT_POST, true);
+        } else {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+        }
+
+        // body
+        if ($body) {
+            $headers['Content-Type'] = 'application/json';
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $body->toJsonString());
+        }
+
+        // header
+        if (!empty($headers)) {
+            $header = [];
+            foreach ($headers as $k => $v) {
+                $header[] = "$k: $v";
+            }
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        }
+
+        // 3. 执行一个cURL会话并且获取相关回复
+        curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+        $response = curl_exec($ch);
+        // $http_code   = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        // 4. 释放cURL句柄,关闭一个cURL会话
+        curl_close($ch);
+
+        return self::parseResponse($response);
+    }
+
+    private static function parseResponse($response)
+    {
+        if ($response === false) {
+            throw new ApiException("curl exec failed.", -1);
+        }
+
+        $statusEnd = strpos($response, "\r\n");
+        $status = substr($response, 0, $statusEnd);
+        $status = explode(' ', $status, 3);
+        $statusCode = intval($status[1] ?? 0);
+
+        if ($status < 200 && $status > 299) {
+            throw new ApiException("response failed.", -2, null, $response);
+        }
+
+        $headerEnd = strpos($response, "\r\n\r\n");
+        $header = substr($response, $statusEnd + 2, $headerEnd - ($statusEnd + 2));
+        $header = explode("\r\n", $header);
+        $headers = [];
+        foreach ($header as $row) {
+            $kw = explode(':', $row, 2);
+            $headers[strtolower($kw[0])] = $kw[1];
+        }
+
+        $body = json_decode(substr($response, $headerEnd + 4), true);
+
+        return [
+            'status' => $status[2],
+            'statusCode' => $statusCode,
+            'headers' => $headers,
+            'body' => $body,
+        ];
+    }
+
+    // 填入路径参数
+    private static function replacePathParams($path, $kw)
+    {
+        $map = [];
+        foreach ($kw as $k => $v) {
+            $map[":$k"] = $v;
+        }
+        $path = str_replace(array_keys($map), $map, $path);
+        return $path;
+    }
+}
---
+++ tools/goctl/api/phpgen/client.go
@@ -0,0 +1,163 @@
+package phpgen
+
+import (
+	_ "embed"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/zeromicro/go-zero/tools/goctl/api/spec"
+)
+
+//go:embed ApiBaseClient.php
+var apiBaseClientTemplate string
+
+//go:embed ApiException.php
+var apiExceptionTemplate string
+
+func genClient(dir string, ns string, api *spec.ApiSpec) error {
+	if err := writeBaseClient(dir, ns); err != nil {
+		return err
+	}
+	if err := writeException(dir, ns); err != nil {
+		return err
+	}
+	return writeClient(dir, ns, api)
+}
+
+func writeBaseClient(dir string, ns string) error {
+	bPath := filepath.Join(dir, "ApiBaseClient.php")
+	bHead := fmt.Sprintf("<?php\n\nnamespace %s;", ns)
+	bSrc := strings.Replace(apiBaseClientTemplate, "<?php", bHead, 1)
+
+	return os.WriteFile(bPath, []byte(bSrc), 0644)
+}
+
+func writeException(dir string, ns string) error {
+	ePath := filepath.Join(dir, "ApiException.php")
+	eHead := fmt.Sprintf("<?php\n\nnamespace %s;", ns)
+	eSrc := strings.Replace(apiExceptionTemplate, "<?php", eHead, 1)
+
+	return os.WriteFile(ePath, []byte(eSrc), 0644)
+}
+
+func writeClient(dir string, ns string, api *spec.ApiSpec) error {
+	name := camelCase(api.Service.Name, true)
+	fp := filepath.Join(dir, fmt.Sprintf("%sClient.php", name))
+	f, err := os.OpenFile(fp, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o644)
+	if err != nil {
+		return err
+	}
+
+	// 头部
+	fmt.Fprintf(f, "<?php \n\nnamespace %s;\n\n", ns)
+
+	// 类
+
+	fmt.Fprintf(f, "class %sClient extends ApiBaseClient {\n", name)
+
+	for _, g := range api.Service.Groups {
+		// 路由
+		for _, r := range g.Routes {
+			an := camelCase(r.Path, true)
+			writeIndent(f, 4)
+			fmt.Fprintf(f, "public function %s%s(", strings.ToLower(r.Method), an)
+			if r.RequestType != nil {
+				fmt.Fprint(f, "$request")
+			}
+			fmt.Fprintln(f, ") {")
+
+			writeIndent(f, 8)
+			fmt.Fprintf(f, "$result = $this->request('%s', '%s',", r.Path, strings.ToLower(r.Method))
+
+			if r.RequestType != nil {
+				params := []string{}
+				for _, tagKey := range tagKeys {
+					if hasTagMembers(r.RequestType, tagKey) {
+						sn := camelCase(fmt.Sprintf("get-%s", tagToSubName(tagKey)), false)
+						params = append(params, fmt.Sprintf("$request->%s()", sn))
+					} else {
+						params = append(params, "null")
+					}
+				}
+				fmt.Fprint(f, strings.Join(params, ","))
+			} else {
+				fmt.Fprint(f, "null, null, null, null")
+			}
+
+			fmt.Fprintln(f, ");")
+
+			writeIndent(f, 8)
+			if r.ResponseType != nil {
+				n := camelCase(r.ResponseType.Name(), true)
+				fmt.Fprintf(f, "$response = new %s();\n", n)
+				definedType, ok := r.ResponseType.(spec.DefineStruct)
+				if !ok {
+					return fmt.Errorf("type %s not supported", n)
+				}
+				if err := writeResponseHeader(f, &definedType); err != nil {
+					return err
+				}
+				if err := writeResponseBody(f, &definedType); err != nil {
+					return err
+				}
+				writeIndent(f, 8)
+				fmt.Fprint(f, "return $response;\n")
+			} else {
+				fmt.Fprint(f, "return null;\n")
+			}
+
+			writeIndent(f, 4)
+			fmt.Fprintln(f, "}")
+		}
+	}
+
+	fmt.Fprintln(f, "}")
+
+	return nil
+}
+
+func writeResponseBody(f *os.File, definedType *spec.DefineStruct) error {
+	// 获取字段
+	ms := definedType.GetTagMembers(bodyTagKey)
+	if len(ms) <= 0 {
+		return nil
+	}
+	writeIndent(f, 8)
+	fmt.Fprint(f, "$response->getBody()")
+	for _, m := range ms {
+		tags := m.Tags()
+		k := ""
+		if len(tags) > 0 {
+			k = tags[0].Name
+		} else {
+			k = m.Name
+		}
+		fmt.Fprintf(f, "\n            ->set%s($result['body']['%s'])", camelCase(m.Name, true), k)
+	}
+	fmt.Fprintln(f, ";")
+	return nil
+}
+
+func writeResponseHeader(f *os.File, definedType *spec.DefineStruct) error {
+	// 获取字段
+	ms := definedType.GetTagMembers(headerTagKey)
+	if len(ms) <= 0 {
+		return nil
+	}
+	writeIndent(f, 8)
+	fmt.Fprint(f, "$response->getHeader()")
+	for _, m := range ms {
+		tags := m.Tags()
+		k := ""
+		if len(tags) > 0 {
+			k = tags[0].Name
+		} else {
+			k = m.Name
+		}
+		fmt.Fprintf(f, "\n            ->set%s($result['header']['%s'])", camelCase(m.Name, true), k)
+	}
+	fmt.Fprintln(f, ";")
+	return nil
+}
