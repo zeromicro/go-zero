89ecb5000521736b5cf30069ce4246582c5d3cc2
kesonan <anqiansong@gmail.com>
2024-12-17 11:59:30 +0800

    remove string restriction on atserver (#4499)

--- tools/goctl/pkg/parser/api/parser/parser.go
+++ tools/goctl/pkg/parser/api/parser/parser.go
@@ -14,8 +14,6 @@ import (
 
 const (
 	idAPI              = "api"
-	summaryKeyExprText = "summary:"
-	summaryKeyText     = "summary"
 	groupKeyText       = "group"
 	infoTitleKey       = "Title"
 	infoDescKey        = "Desc"
@@ -1201,12 +1199,6 @@ func (p *Parser) parseAtServerKVExpression() *ast.KVExpr {
 		expr.Value = node
 		return expr
 	} else if p.peekTokenIs(token.STRING) {
-		if expr.Key.Token.Text != summaryKeyExprText {
-			if p.notExpectPeekToken(token.QUO, token.DURATION, token.IDENT, token.INT) {
-				return nil
-			}
-		}
-
 		if !p.nextToken() {
 			return nil
 		}
