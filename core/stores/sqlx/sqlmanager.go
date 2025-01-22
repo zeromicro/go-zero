d85b179ff4c93368feb33b4064a6fbac91a58c5e
kevin <wanjunfeng@gmail.com>
2025-01-22 20:15:07 +0800

    chore: only upload metrics on mysql

--- core/stores/sqlx/sqlmanager.go
+++ core/stores/sqlx/sqlmanager.go
@@ -27,19 +27,21 @@ func getCachedSqlConn(driverName, server string) (*sql.DB, error) {
 			return nil, err
 		}
 
-		if cfg, e := mysql.ParseDSN(server); e != nil {
-			// if cannot parse, don't collect the metrics
-			logx.Error(e)
-		} else {
-			checksum := sha256.Sum256([]byte(server))
-			connCollector.registerClient(&statGetter{
-				host:   cfg.Addr,
-				dbName: cfg.DBName,
-				hash:   hex.EncodeToString(checksum[:]),
-				poolStats: func() sql.DBStats {
-					return conn.Stats()
-				},
-			})
+		if driverName != mysqlDriverName {
+			if cfg, e := mysql.ParseDSN(server); e != nil {
+				// if cannot parse, don't collect the metrics
+				logx.Error(e)
+			} else {
+				checksum := sha256.Sum256([]byte(server))
+				connCollector.registerClient(&statGetter{
+					host:   cfg.Addr,
+					dbName: cfg.DBName,
+					hash:   hex.EncodeToString(checksum[:]),
+					poolStats: func() sql.DBStats {
+						return conn.Stats()
+					},
+				})
+			}
 		}
 
 		return conn, nil
