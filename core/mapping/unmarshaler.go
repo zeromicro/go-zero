b20da4074b0b5e0a15b69533b5958b0473141d5c
kevin <wanjunfeng@gmail.com>
2025-06-13 22:03:44 +0800

    chore: add more tests

--- core/mapping/unmarshaler.go
+++ core/mapping/unmarshaler.go
@@ -622,13 +622,19 @@ func (u *Unmarshaler) processFieldNotFromString(fieldType reflect.Type, value re
 
 		return u.fillSliceFromString(fieldType, value, mapValue, fullName)
 	case valueKind == reflect.String && derefedFieldType == durationType:
-		v, ok := mapValue.(string)
-		if !ok {
-			return fmt.Errorf("unexpected type %T, expected a string value for field %s", mapValue, fullName)
+		v, err := convertToString(mapValue, fullName)
+		if err != nil {
+			return err
 		}
+
 		return fillDurationValue(fieldType, value, v)
 	case valueKind == reflect.String && typeKind == reflect.Struct && u.implementsUnmarshaler(fieldType):
-		return u.fillUnmarshalerStruct(fieldType, value, mapValue.(string))
+		v, err := convertToString(mapValue, fullName)
+		if err != nil {
+			return err
+		}
+
+		return u.fillUnmarshalerStruct(fieldType, value, v)
 	default:
 		return u.processFieldPrimitive(fieldType, value, mapValue, opts, fullName)
 	}
