d69d0429dd0583fa5fd32a3276e98d61df3f188b
soasurs <soasurs@gmail.com>
2025-07-08 16:02:34 +0800

    test(zrpc/resolver): update EventHandler unit tests to use discovery/v1 EndpointSlice
    * Replace legacy core/v1 Endpoints fixtures with discovery/v1 EndpointSlice
    * Adapt test data: switch Subsets/Addresses to Endpoints[].Addresses[]
    * Preserve existing test scenarios for add, delete, and update event flows
    
    Signed-off-by: soasurs <soasurs@gmail.com>

--- zrpc/resolver/internal/kube/eventhandler_test.go
+++ zrpc/resolver/internal/kube/eventhandler_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	v1 "k8s.io/api/core/v1"
+	discoveryv1 "k8s.io/api/discovery/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -14,21 +14,19 @@ func TestAdd(t *testing.T) {
 		endpoints = change
 	})
 	h.OnAdd("bad", false)
-	h.OnAdd(&v1.Endpoints{Subsets: []v1.EndpointSubset{
-		{
-			Addresses: []v1.EndpointAddress{
-				{
-					IP: "0.0.0.1",
-				},
-				{
-					IP: "0.0.0.2",
-				},
-				{
-					IP: "0.0.0.3",
-				},
+	h.OnAdd(&discoveryv1.EndpointSlice{
+		Endpoints: []discoveryv1.Endpoint{
+			{
+				Addresses: []string{"0.0.0.1"},
+			},
+			{
+				Addresses: []string{"0.0.0.2"},
+			},
+			{
+				Addresses: []string{"0.0.0.3"},
 			},
 		},
-	}}, false)
+	}, false)
 	assert.ElementsMatch(t, []string{"0.0.0.1", "0.0.0.2", "0.0.0.3"}, endpoints)
 }
 
@@ -37,34 +35,30 @@ func TestDelete(t *testing.T) {
 	h := NewEventHandler(func(change []string) {
 		endpoints = change
 	})
-	h.OnAdd(&v1.Endpoints{Subsets: []v1.EndpointSubset{
-		{
-			Addresses: []v1.EndpointAddress{
-				{
-					IP: "0.0.0.1",
-				},
-				{
-					IP: "0.0.0.2",
-				},
-				{
-					IP: "0.0.0.3",
-				},
+	h.OnAdd(&discoveryv1.EndpointSlice{
+		Endpoints: []discoveryv1.Endpoint{
+			{
+				Addresses: []string{"0.0.0.1"},
+			},
+			{
+				Addresses: []string{"0.0.0.2"},
+			},
+			{
+				Addresses: []string{"0.0.0.3"},
 			},
 		},
-	}}, false)
+	}, false)
 	h.OnDelete("bad")
-	h.OnDelete(&v1.Endpoints{Subsets: []v1.EndpointSubset{
-		{
-			Addresses: []v1.EndpointAddress{
-				{
-					IP: "0.0.0.1",
-				},
-				{
-					IP: "0.0.0.2",
-				},
+	h.OnDelete(&discoveryv1.EndpointSlice{
+		Endpoints: []discoveryv1.Endpoint{
+			{
+				Addresses: []string{"0.0.0.1"},
+			},
+			{
+				Addresses: []string{"0.0.0.2"},
 			},
 		},
-	}})
+	})
 	assert.ElementsMatch(t, []string{"0.0.0.3"}, endpoints)
 }
 
@@ -73,36 +67,28 @@ func TestUpdate(t *testing.T) {
 	h := NewEventHandler(func(change []string) {
 		endpoints = change
 	})
-	h.OnUpdate(&v1.Endpoints{
-		Subsets: []v1.EndpointSubset{
+	h.OnUpdate(&discoveryv1.EndpointSlice{
+		Endpoints: []discoveryv1.Endpoint{
 			{
-				Addresses: []v1.EndpointAddress{
-					{
-						IP: "0.0.0.1",
-					},
-					{
-						IP: "0.0.0.2",
-					},
-				},
+				Addresses: []string{"0.0.0.1"},
+			},
+			{
+				Addresses: []string{"0.0.0.2"},
 			},
 		},
 		ObjectMeta: metav1.ObjectMeta{
 			ResourceVersion: "1",
 		},
-	}, &v1.Endpoints{
-		Subsets: []v1.EndpointSubset{
+	}, &discoveryv1.EndpointSlice{
+		Endpoints: []discoveryv1.Endpoint{
 			{
-				Addresses: []v1.EndpointAddress{
-					{
-						IP: "0.0.0.1",
-					},
-					{
-						IP: "0.0.0.2",
-					},
-					{
-						IP: "0.0.0.3",
-					},
-				},
+				Addresses: []string{"0.0.0.1"},
+			},
+			{
+				Addresses: []string{"0.0.0.2"},
+			},
+			{
+				Addresses: []string{"0.0.0.3"},
 			},
 		},
 		ObjectMeta: metav1.ObjectMeta{
@@ -116,33 +102,25 @@ func TestUpdateNoChange(t *testing.T) {
 	h := NewEventHandler(func(change []string) {
 		assert.Fail(t, "should not called")
 	})
-	h.OnUpdate(&v1.Endpoints{
-		Subsets: []v1.EndpointSubset{
+	h.OnUpdate(&discoveryv1.EndpointSlice{
+		Endpoints: []discoveryv1.Endpoint{
 			{
-				Addresses: []v1.EndpointAddress{
-					{
-						IP: "0.0.0.1",
-					},
-					{
-						IP: "0.0.0.2",
-					},
-				},
+				Addresses: []string{"0.0.0.1"},
+			},
+			{
+				Addresses: []string{"0.0.0.2"},
 			},
 		},
 		ObjectMeta: metav1.ObjectMeta{
 			ResourceVersion: "1",
 		},
-	}, &v1.Endpoints{
-		Subsets: []v1.EndpointSubset{
+	}, &discoveryv1.EndpointSlice{
+		Endpoints: []discoveryv1.Endpoint{
 			{
-				Addresses: []v1.EndpointAddress{
-					{
-						IP: "0.0.0.1",
-					},
-					{
-						IP: "0.0.0.2",
-					},
-				},
+				Addresses: []string{"0.0.0.1"},
+			},
+			{
+				Addresses: []string{"0.0.0.2"},
 			},
 		},
 		ObjectMeta: metav1.ObjectMeta{
@@ -156,45 +134,35 @@ func TestUpdateChangeWithDifferentVersion(t *testing.T) {
 	h := NewEventHandler(func(change []string) {
 		endpoints = change
 	})
-	h.OnAdd(&v1.Endpoints{Subsets: []v1.EndpointSubset{
-		{
-			Addresses: []v1.EndpointAddress{
-				{
-					IP: "0.0.0.1",
-				},
-				{
-					IP: "0.0.0.3",
-				},
+	h.OnAdd(&discoveryv1.EndpointSlice{
+		Endpoints: []discoveryv1.Endpoint{
+			{
+				Addresses: []string{"0.0.0.1"},
+			},
+			{
+				Addresses: []string{"0.0.0.3"},
 			},
 		},
-	}}, false)
-	h.OnUpdate(&v1.Endpoints{
-		Subsets: []v1.EndpointSubset{
+	}, false)
+	h.OnUpdate(&discoveryv1.EndpointSlice{
+		Endpoints: []discoveryv1.Endpoint{
 			{
-				Addresses: []v1.EndpointAddress{
-					{
-						IP: "0.0.0.1",
-					},
-					{
-						IP: "0.0.0.3",
-					},
-				},
+				Addresses: []string{"0.0.0.1"},
+			},
+			{
+				Addresses: []string{"0.0.0.3"},
 			},
 		},
 		ObjectMeta: metav1.ObjectMeta{
 			ResourceVersion: "1",
 		},
-	}, &v1.Endpoints{
-		Subsets: []v1.EndpointSubset{
+	}, &discoveryv1.EndpointSlice{
+		Endpoints: []discoveryv1.Endpoint{
 			{
-				Addresses: []v1.EndpointAddress{
-					{
-						IP: "0.0.0.1",
-					},
-					{
-						IP: "0.0.0.2",
-					},
-				},
+				Addresses: []string{"0.0.0.1"},
+			},
+			{
+				Addresses: []string{"0.0.0.2"},
 			},
 		},
 		ObjectMeta: metav1.ObjectMeta{
@@ -209,63 +177,49 @@ func TestUpdateNoChangeWithDifferentVersion(t *testing.T) {
 	h := NewEventHandler(func(change []string) {
 		endpoints = change
 	})
-	h.OnAdd(&v1.Endpoints{Subsets: []v1.EndpointSubset{
-		{
-			Addresses: []v1.EndpointAddress{
-				{
-					IP: "0.0.0.1",
-				},
-				{
-					IP: "0.0.0.2",
-				},
-			},
-		},
-	}}, false)
-	h.OnUpdate("bad", &v1.Endpoints{Subsets: []v1.EndpointSubset{
-		{
-			Addresses: []v1.EndpointAddress{
-				{
-					IP: "0.0.0.1",
-				},
-			},
-		},
-	}})
-	h.OnUpdate(&v1.Endpoints{Subsets: []v1.EndpointSubset{
-		{
-			Addresses: []v1.EndpointAddress{
-				{
-					IP: "0.0.0.1",
-				},
-			},
-		},
-	}}, "bad")
-	h.OnUpdate(&v1.Endpoints{
-		Subsets: []v1.EndpointSubset{
+	h.OnAdd(&discoveryv1.EndpointSlice{
+		Endpoints: []discoveryv1.Endpoint{
 			{
-				Addresses: []v1.EndpointAddress{
-					{
-						IP: "0.0.0.1",
-					},
-					{
-						IP: "0.0.0.2",
-					},
-				},
+				Addresses: []string{"0.0.0.1"},
+			},
+			{
+				Addresses: []string{"0.0.0.2"},
+			},
+		},
+	}, false)
+	h.OnUpdate("bad", &discoveryv1.EndpointSlice{
+		Endpoints: []discoveryv1.Endpoint{
+			{
+				Addresses: []string{"0.0.0.1"},
+			},
+		},
+	})
+	h.OnUpdate(&discoveryv1.EndpointSlice{
+		Endpoints: []discoveryv1.Endpoint{
+			{
+				Addresses: []string{"0.0.0.1"},
+			},
+		},
+	}, "bad")
+	h.OnUpdate(&discoveryv1.EndpointSlice{
+		Endpoints: []discoveryv1.Endpoint{
+			{
+				Addresses: []string{"0.0.0.1"},
+			},
+			{
+				Addresses: []string{"0.0.0.2"},
 			},
 		},
 		ObjectMeta: metav1.ObjectMeta{
 			ResourceVersion: "1",
 		},
-	}, &v1.Endpoints{
-		Subsets: []v1.EndpointSubset{
+	}, &discoveryv1.EndpointSlice{
+		Endpoints: []discoveryv1.Endpoint{
 			{
-				Addresses: []v1.EndpointAddress{
-					{
-						IP: "0.0.0.1",
-					},
-					{
-						IP: "0.0.0.2",
-					},
-				},
+				Addresses: []string{"0.0.0.1"},
+			},
+			{
+				Addresses: []string{"0.0.0.2"},
 			},
 		},
 		ObjectMeta: metav1.ObjectMeta{
