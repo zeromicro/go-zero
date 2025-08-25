4284f9d72db11495f5fb45fa1136c987764e0038
WqyJh <781345688@qq.com>
2025-08-25 16:20:25 +0800

    feat(goctl): add default template func map utility

--- tools/goctl/util/templatex_test.go
+++ tools/goctl/util/templatex_test.go
@@ -1,8 +1,11 @@
 package util
 
 import (
+	"bytes"
 	"testing"
+	"text/template"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/zeromicro/go-zero/tools/goctl/test"
 )
 
@@ -90,4 +93,40 @@ func TestTemplateVariable(t *testing.T) {
 		},
 	}...)
 	executor.Run(t, TemplateVariable)
+}
+
+func TestTemplateFuncMap(t *testing.T) {
+	cases := []struct {
+		name     string
+		funcMap  template.FuncMap
+		template string
+		variable map[string]any
+		want     string
+	}{
+		{
+			name:     "upper",
+			template: "{{.foo | upper}}",
+			variable: map[string]any{"foo": "bar"},
+			want:     "BAR",
+		},
+		{
+			name:     "repeat upper",
+			template: "{{.foo | repeat 3 | upper}}",
+			variable: map[string]any{"foo": "bar"},
+			want:     "BARBARBAR",
+		},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			funcMap := DefaultFuncMap()
+			if c.funcMap != nil {
+				funcMap = MergeWithDefaultFuncMap(c.funcMap)
+			}
+			tpl := template.Must(template.New("test").Funcs(funcMap).Parse(c.template))
+			var buf bytes.Buffer
+			err := tpl.Execute(&buf, c.variable)
+			assert.NoError(t, err)
+			assert.Equal(t, c.want, buf.String())
+		})
+	}
 }
